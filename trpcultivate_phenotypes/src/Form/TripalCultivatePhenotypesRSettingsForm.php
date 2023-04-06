e4d99902f3fc8079debf46e555aceea1955bd658
reynold tan <reynoldltan@gmail.com>
2023-04-06 15:08:26 -0600

    R Rules configuration page ok! @TODO: fix failed test

--- trpcultivate_phenotypes/src/Form/TripalCultivatePhenotypesRSettingsForm.php
+++ trpcultivate_phenotypes/src/Form/TripalCultivatePhenotypesRSettingsForm.php
@@ -31,8 +31,45 @@ class TripalCultivatePhenotypesRSettingsForm extends ConfigFormBase {
       static::SETTINGS,
     ];
   }  
-
+  
+  /** 
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config(static::SETTINGS);
+    $config_r = 'trpcultivate.phenotypes.r_config.';
+
+    $form['description'] = [
+      '#markup' => $this->t('Tripal Cultivate Phenotypes supports R language for statistical computing by providing
+      syntactically valid version of trait or values. Use R Transformation Rules configuration page to define 
+      standard transformation rules to apply to trait or string when converting to R version.')
+    ];
+
+    $form['words'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('List of words to remove'),
+      '#default_value' => $config->get($config_r . 'words'),
+      '#description' => $this->t('Separate each word entry with a comma character'),
+      '#required' => TRUE,
+    ];
+
+    $form['chars'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('List of special characters to remove'),
+      '#default_value' => $config->get($config_r . 'chars'),
+      '#description' => $this->t('Separate each character entry with a comma character'),
+      '#required' => TRUE,
+    ];
+
+    $form['replace'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Match word and replace with'),
+      '#default_value' => $config->get($config_r . 'replace'),
+      '#description' => $this->t('Separate match and replace pairs with a comma character'),
+      '#required' => TRUE,
+    ];
+
+    
     return parent::buildForm($form, $form_state);
   }
 
@@ -40,6 +77,14 @@ class TripalCultivatePhenotypesRSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config_r = 'trpcultivate.phenotypes.r_config.';
+
+    $this->configFactory->getEditable(static::SETTINGS)
+      ->set($config_r . 'words', $form_state->getValue('words'))
+      ->set($config_r . 'chars', $form_state->getValue('chars'))
+      ->set($config_r . 'replace', $form_state->getValue('replace'))
+      ->save();
+
     return parent::submitForm($form, $form_state);
   }
 }
--- trpcultivate_phenotypes/tests/src/Functional/InstallTest.php
+++ trpcultivate_phenotypes/tests/src/Functional/InstallTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\Tests\trpgeno_genetics\Functional;
+namespace Drupal\Tests\trpcultivate_phenotypes\Functional;
 
 use Drupal\Core\Url;
 use Drupal\Tests\tripal_chado\Functional\ChadoTestBrowserBase;
@@ -8,7 +8,7 @@ use Drupal\Tests\tripal_chado\Functional\ChadoTestBrowserBase;
 /**
  * Simple test to ensure that main page loads with module enabled.
  *
- * @group TripGeno Genetics
+ * @group TripPheno Phenotypes
  * @group Installation
  */
 class InstallTest extends ChadoTestBrowserBase {
@@ -20,7 +20,7 @@ class InstallTest extends ChadoTestBrowserBase {
    *
    * @var array
    */
-  protected static $modules = ['help', 'trpcultivate_phenotypes'];
+  protected static $modules = ['trpcultivate_phenotypes'];
 
   /**
    * The name of your module in the .info.yml
@@ -33,19 +33,13 @@ class InstallTest extends ChadoTestBrowserBase {
   protected static $module_machinename = 'trpcultivate_phenotypes';
 
   /**
-   * A small excert from your help page.
-   * Do not cross newlines.
-   */
-  protected static $help_text_excerpt = 'pports collecting all data for a specific trait (e.g. Plant Height) into a single';
-
-  /**
    * Tests that a specific set of pages load with a 200 response.
    */
   public function testLoad() {
     $session = $this->getSession();
 
     // Ensure we have an admin user.
-    $user = $this->drupalCreateUser(['access administration pages', 'administer modules']);
+    $user = $this->drupalCreateUser(['access administration pages', 'administer modules', 'administer tripal']);
     $this->drupalLogin($user);
 
     $context = '(modules installed: ' . implode(',', self::$modules) . ')';
@@ -60,40 +54,5 @@ class InstallTest extends ChadoTestBrowserBase {
     $status_code = $session->getStatusCode();
     $this->assertEquals(200, $status_code, "The module install page should be able to load $context.");
     $this->assertSession()->pageTextContains( self::$module_name );
-
   }
-
-  /**
-   * Tests the module overview help.
-   */
-  public function testHelp() {
-    $session = $this->getSession();
-
-    $some_extected_text = self::$help_text_excerpt;
-
-    // Ensure we have an admin user.
-    $user = $this->drupalCreateUser(['access administration pages', 'administer modules']);
-    $this->drupalLogin($user);
-
-    $context = '(modules installed: ' . implode(',', self::$modules) . ')';
-
-    // Call the hook to ensure it is returning text.
-    $name = 'help.page.' . $this::$module_machinename;
-    $match = $this->createStub(\Drupal\Core\Routing\RouteMatch::class);
-    $hook_name = self::$module_machinename . '_help';
-    $output = $hook_name($name, $match);
-    $this->assertNotEmpty($output, "The help hook should return output $context.");
-    $this->assertStringContainsString($some_extected_text, $output);
-
-    // Help Page.
-    $this->drupalGet('admin/help');
-    $status_code = $session->getStatusCode();
-    $this->assertEquals(200, $status_code, "The admin help page should be able to load $context.");
-    $this->assertSession()->pageTextContains(self::$module_name);
-    $this->drupalGet('admin/help/' . self::$module_machinename);
-    $status_code = $session->getStatusCode();
-    $this->assertEquals(200, $status_code, "The module help page should be able to load $context.");
-    $this->assertSession()->pageTextContains($some_extected_text);
-  }
-
 }
