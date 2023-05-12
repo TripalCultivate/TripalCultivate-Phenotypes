355e9c562c28427ff1cd8fe6975c6fff888e3fde
reynold tan <reynoldltan@gmail.com>
2023-05-12 13:42:45 -0600

    Tests validate and build form @todo: test form submit

--- trpcultivate_phenotypes/tests/src/Unit/ConfigRRulesFormTest.php
+++ trpcultivate_phenotypes/tests/src/Unit/ConfigRRulesFormTest.php
@@ -22,7 +22,8 @@ use Drupal\Core\DependencyInjection\ContainerBuilder;
   * @group trpcultivate_phenotypes
   */
 class ConfigRRulesFormTest extends UnitTestCase {
-  
+  private $rrulesform;
+
   /**
    * Initialization of container, configurations, service 
    * and service class required by the test.
@@ -37,12 +38,19 @@ class ConfigRRulesFormTest extends UnitTestCase {
     // Mock config since the RRules form extends configFormBase and expects
     // a configuration settings.
     $r_config_mock = $this->prophesize(Config::class);
-    $r_config_mock->get('trpcultivate.phenotypes.r_config')->willReturn([
-      'words' => ['of', 'to', 'have', 'on', 'at'],
-      'chars' => ['(', ')', '/', '-', ':', ';', '%'],
-      'replace' => [ '# = num', '/ = div', '? = unsure', '- = to']
+
+    $r_config_mock->get('trpcultivate.phenotypes.r_config.chars')->willReturn([
+      '(', ')', '/', '-', ':', ';', '%'
     ]);
-   
+    
+    $r_config_mock->get('trpcultivate.phenotypes.r_config.words')->willReturn([
+      'of', 'to', 'have', 'on', 'at'
+    ]);
+
+    $r_config_mock->get('trpcultivate.phenotypes.r_config.replace')->willReturn([
+      '# = num', '/ = div', '? = unsure', '- = to'
+    ]);
+    
     // When RRules form rebuilds calling the module settings, return
     // only the R configuration settings above exclude other config.
     $all_config_mock = $this->prophesize(ConfigFactoryInterface::class);
@@ -57,21 +65,58 @@ class ConfigRRulesFormTest extends UnitTestCase {
     $translation = $translation_mock->reveal();
 
     // Class RRulesForm class instance.
+    
     $rrules_form = new TripalCultivatePhenotypesRSettingsForm($r_config);
     $rrules_form->setStringTranslation($translation);
 
     $container->set('rrules.config', $rrules_form);
+    $this->rrulesform = \Drupal::service('rrules.config');
   }
+  
+  /**
+   * Test submit form functionality of RRulesForm class.
+   */  
+  /*
+   public function testSubmitForm() {
+    $form = [];
+    $form_state = new FormState();
+    
+    $form_state->setValue('words', 'num,log');
+    $form_state->setValue('chars', '#,*');
+    $form_state->setValue('words', 'num,log');
+    
+    $this->rrulesform->submitForm($form, $form_state);
+  }*/
+
+  /**
+   * Test build form functionality of RRulesForm class.
+   */  
+  public function testFormId() {
+    // Test if it is the RRules config form using the form id.
+    $this->assertEquals('trpcultivate_phenotypes_r_settings_form', $this->rrulesform->getFormId()); 
+  }
+
+  /**
+   * Test build form functionality of RRulesForm class.
+   */  
+  public function testBuildForm() {
+    $form = [];
+    $form_state = new FormState();
+    $config_form = $this->rrulesform->buildForm($form, $form_state);
+  
+    // Form theme is system configuration type.
+    $this->assertEquals('system_config_form', $config_form['#theme']);
+    // Field types.
+    $this->assertEquals('textarea', $config_form['words']['#type']);
+    $this->assertEquals('textarea', $config_form['chars']['#type']);
+    $this->assertEquals('textarea', $config_form['replace']['#type']);
+  }
+
 
   /**
    * Test validate functionality of RRulesForm class.
    */  
   public function testValidateForm() {
-    $rrules = \Drupal::service('rrules.config');
-
-    // Test if it is the RRules config form using the form id.
-    $this->assertEquals('trpcultivate_phenotypes_r_settings_form', $rrules->getFormId());
-    
     // Method formValidate requires 2 parameters $form and $form_state.
     $form_state = new FormState();
     $form = [];
@@ -82,17 +127,17 @@ class ConfigRRulesFormTest extends UnitTestCase {
     $field = 'words';
     foreach(['R', 'r', '.', '~', '1', '123', ' '] as $rule) {
       $form_state->setValue($field, $rule);
-      $rrules->validateForm($form, $form_state);
+      $this->rrulesform->validateForm($form, $form_state);
 
       $this->assertTrue($form_state->hasAnyErrors(), $rule);
     }
 
-    $form_state->clearErrors();
 
     // Valid words:
     foreach(['Hello', 'hello', 'plant', 'seeds', 'this', 'that', 'no'] as $rule) {
+      $form_state->clearErrors();
       $form_state->setValue($field, $rule);
-      $rrules->validateForm($form, $form_state);
+      $this->rrulesform->validateForm($form, $form_state);
 
       $this->assertFalse($form_state->hasAnyErrors(), $rule);
     }
@@ -104,17 +149,17 @@ class ConfigRRulesFormTest extends UnitTestCase {
     $field = 'chars';
     foreach(['hello', ',', 'A', 'a', '1', 0, ' '] as $rule) {
       $form_state->setValue($field, $rule);
-      $rrules->validateForm($form, $form_state);
+      $this->rrulesform->validateForm($form, $form_state);
 
       $this->assertTrue($form_state->hasAnyErrors(), $rule);
     }
 
-    $form_state->clearErrors();
 
     // Valid words:
-    foreach(['~', '@', '>', '+', '-', '$', ':'] as $rule) {
+    foreach(['~', '@', '>', '+', '-', '$', ':'] as $rule) {      
+      $form_state->clearErrors();
       $form_state->setValue($field, $rule);
-      $rrules->validateForm($form, $form_state);
+      $this->rrulesform->validateForm($form, $form_state);
 
       $this->assertFalse($form_state->hasAnyErrors(), $rule);
     }
@@ -127,19 +172,21 @@ class ConfigRRulesFormTest extends UnitTestCase {
     $field = 'replace';
     foreach([' ',', = a', 'hi=hello', 'hi= hello', 'hi => hello'] as $rule) {
       $form_state->setValue($field, $rule);
-      $rrules->validateForm($form, $form_state);
+      $this->rrulesform->validateForm($form, $form_state);
 
       $this->assertTrue($form_state->hasAnyErrors(), $rule);
     }
 
-    $form_state->clearErrors();
 
     // Valid words:
     foreach(['hi = hello', 'yes = no', 'a = b'] as $rule) {
+      $form_state->clearErrors();
       $form_state->setValue($field, $rule);
-      $rrules->validateForm($form, $form_state);
+      $this->rrulesform->validateForm($form, $form_state);
 
       $this->assertFalse($form_state->hasAnyErrors(), $rule);
     }
   } 
+
+
 }
