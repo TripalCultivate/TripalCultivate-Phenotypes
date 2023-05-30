b4ef4c328b9e536e71e6fff77a4d59892b39e770
Reynold Tan <reynoldltan@gmail.com>
2023-05-30 16:13:23 -0600

    Functional test OK! @TODO: unit test

--- trpcultivate_phenotypes/tests/src/Functional/ConfigOntologyTest.php
+++ trpcultivate_phenotypes/tests/src/Functional/ConfigOntologyTest.php
@@ -51,6 +51,9 @@ class ConfigOntologyTest extends BrowserTestBase {
     // Ensure we see all logging in tests.
     \Drupal::state()->set('is_a_test_environment', TRUE);
 
+    $srv_terms = \Drupal::service('trpcultivate_phenotypes.terms');
+    $srv_ontology = \Drupal::service('trpcultivate_phenotypes.ontology');
+
     // Setup admin user account.
     $admin_user = $this->drupalCreateUser([
       'administer site configuration',
@@ -66,33 +69,85 @@ class ConfigOntologyTest extends BrowserTestBase {
 
     $session->statusCodeEquals(200);
     $session->pageTextContains('Configure Tripal Cultivate Phenotypes: Ontology Terms');
+    
+    // TERMS:
+    // Install sequence set all terms used by this module to default 
+    // term values. This test will set all said values to null.
+    // null has cvterm_id of 1.
+    $null_value = 1;
 
-    /*
-    // Update configuration settings.
-    // Do not watermark any charts.
-    $update_watermark = [
-      'charts' => 0,
-      // 'file' => '' test could not detect file field. ??
+    $terms = $srv_terms->mapDefaultTermToConfig();
+    $term_form_values = [];
+
+    // Imitate value returned by autocomplete field in the following
+    // format Term name (Db name:Dbxref accession).
+    $null_term = $srv_terms->getTerm($null_value);
+    $format_null = $null_term['format'];
+    
+    // Set the fields.
+    foreach($terms as $term => $config) {
+      $term_form_values[ $config ] = $format_null;
+    }
+
+    $this->submitForm($term_form_values, 'Save configuration');    
+    
+    // Test if all terms got null-ed.
+    foreach($terms as $term => $config) {
+      $this->assertSession()->fieldValueEquals($config, $format_null);
+    }
+
+    // ALLOW NEW TRAIT:
+    // Allow new traits to be added during the upload process.
+    // By default, allow new trait is set to TRUE - allow.
+    $allow_new = 'allow_new';
+    $this->submitForm([$allow_new => FALSE], 'Save configuration');    
+    $this->assertSession()->fieldValueEquals($allow_new, FALSE);
+    
+
+    // GENUS-ONTOLOGY:
+    // Create a genus and set the cv for trait, method and unit to null,
+    // database to null and finally, crop ontology to null as well.
+    
+    // Ensure we see all logging in tests.
+    \Drupal::state()->set('is_a_test_environment', TRUE);
+    $chado = \Drupal::service('tripal_chado.database');
+    
+    // Lens culinaris, null type.
+    $insert = "INSERT INTO {1:organism} (genus, species, type_id) 
+      VALUES ('Lens', 'culinaris', 1)";
+    
+    $chado->query($insert);
+
+    $lens = $chado->query("SELECT organism_id FROM {1:organism} WHERE genus = 'Lens' LIMIT 1")
+      ->fetchField();
+    
+    // Install this organism - prepare a genus-ontology configuration.
+    $srv_ontology->loadGenusOntology();
+
+    // Inspect if genus-ontology configuration was set.
+    $genus_ontology = $srv_ontology->getGenusOntologyConfigValue('Lens');
+    unset($config);
+    foreach($genus_ontology as $config => $value) {
+      $this->assertEquals($value, 0);
+    }
+
+    // Set the value to some cvs and some dbs of genus - Lens.
+    $ontology_form_values = [
+      'lens_trait' => $null_value,         // Null, no vocabulary cv
+      'lens_method' => $null_value,        // Null, no vocabulary cv
+      'lens_unit'    =>  $null_value,      // Null, no vocabulary cv
+      'lens_database' => 1,                // Null, no database
+      'lens_crop_ontology' => $null_value  // Null, no vocabulary cv
     ];
 
-    // Fields and default value.
-    $session->fieldExists('charts');
-    $session->fieldValueEquals('charts', 0);
-
-    // Could not seem to find this field in the form.
-    // $session->fieldExists('file');
-    // $session->fieldValueEquals('file', '');
-
-    // Submit form.
-    $this->submitForm($update_watermark, 'Save configuration');
-
-    // Assert configuration saved.
-    $this->assertRaw('The configuration options have been saved.');
-
-    // Assert Fields reflect the updated configuration.
-    $session->fieldValueEquals('charts', $update_watermark['charts']);
+    $this->submitForm($ontology_form_values, 'Save configuration');    
     
+    unset($config);
+    foreach($genus_ontology as $config => $value) {
+      // All genus ontology config set to Null cv/db.
+      $this->assertSession()->fieldValueEquals('lens_' . $config, $null_value);
+    }
+
     $this->drupalLogout();
-    */
   }
 }
