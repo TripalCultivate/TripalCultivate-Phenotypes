6ed7f6c8d57fe044b9030c37b43210e3c38781ba
Lacey Sanderson <laceyannesanderson@gmail.com>
2023-06-02 13:48:55 -0600

    Work with config for testing.

--- trpcultivate_phenotypes/tests/src/Unit/ConfigWatermarkFormTest.php
+++ trpcultivate_phenotypes/tests/src/Unit/ConfigWatermarkFormTest.php
@@ -13,6 +13,8 @@ use Drupal\Core\Form\FormState;
 use Drupal\Tests\UnitTestCase;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use \Drupal\Core\File\FileUrlGeneratorInterface;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 
 /**
@@ -23,7 +25,7 @@ use Drupal\Core\DependencyInjection\ContainerBuilder;
   */
 class ConfigWatermarkFormTest extends UnitTestCase {
   /**
-   * Initialization of container, configurations, service 
+   * Initialization of container, configurations, service
    * and service class required by the test.
    */
   protected function setUp(): void {
@@ -41,7 +43,9 @@ class ConfigWatermarkFormTest extends UnitTestCase {
       'image' => null,
       'file_ext' => ['png', 'gif']
     ]);
-   
+    $watermark_config_mock->get('trpcultivate.phenotypes.directory.watermark')
+      ->willReturn('public://TripalCultivatePhenotypes/watermark/');
+
     // When watermark form rebuilds calling the module settings, return
     // only the watertmark configuration settings above exclude other config.
     $all_config_mock = $this->prophesize(ConfigFactoryInterface::class);
@@ -51,26 +55,51 @@ class ConfigWatermarkFormTest extends UnitTestCase {
     // Isolated configuration for watermark configuration.
     $watermark_config = $all_config_mock->reveal();
 
-    // Translation requirement of the container
-    $translation_mock = $this->prophesize(TranslationInterface::class);
-    $translation = $translation_mock->reveal();
-
     // Class WatermarkForm class instance.
     $watermark_form = new TripalCultivatePhenotypesWatermarkSettingsForm($watermark_config);
+
+    // Requirement of the container
+    //  -- Translation
+    $mock = $this->prophesize(TranslationInterface::class);
+    $translation = $mock->reveal();
     $watermark_form->setStringTranslation($translation);
+    //  -- Messenger.
+    $mock = $this->prophesize(MessengerInterface::class);
+    $messenger = $mock->reveal();
+    $watermark_form->setMessenger($messenger);
+    //  -- File URL Generator.
+    $mock = $this->prophesize(FileUrlGeneratorInterface::class);
+    $fileGenerator = $mock->reveal();
+    $container->set('file_url_generator', $fileGenerator);
 
     $container->set('watermark.config', $watermark_form);
   }
 
   /**
+   * Test build form functionality of RRulesForm class.
+   */
+  public function testBuildForm() {
+    $watermark = \Drupal::service('watermark.config');
+
+    $form = [];
+    $form_state = new FormState();
+    $config_form = $watermark->buildForm($form, $form_state);
+
+    // Form theme is system configuration type.
+    $this->assertEquals('system_config_form', $config_form['#theme']);
+    // Field types.
+    $this->assertEquals('radios', $config_form['charts']['#type']);
+  }
+
+  /**
    * Test validate functionality of WatermarkForm class.
-   */  
+   */
   public function testValidateForm() {
     $watermark = \Drupal::service('watermark.config');
 
     // Test if it is the watermark config form using the form id.
     $this->assertEquals('trpcultivate_phenotypes_watermark_settings_form', $watermark->getFormId());
-    
+
     // Method formValidate requires 2 parameters $form and $form_state.
     $form_state = new FormState();
     $form = [];
@@ -88,5 +117,5 @@ class ConfigWatermarkFormTest extends UnitTestCase {
     $form_state->setValue('file', []);
     $watermark->validateForm($form, $form_state);
     $this->assertFalse($form_state->hasAnyErrors());
-  } 
+  }
 }
