f569c0c4f5aa0ca12f790bd20fef0f47cd58390e
reynold tan <reynoldltan@gmail.com>
2023-06-29 11:03:48 -0600

    Fixes failed test: refactor test codebase

--- trpcultivate_phenotypes/tests/src/Kernel/ServiceTermTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/ServiceTermTest.php
@@ -16,48 +16,43 @@
   * @group trpcultivate_phenotypes
   */
 class ServiceTermTest extends KernelTestBase {
+  /**
+   * Term service.
+   * 
+   * @var object
+   */
   protected $service;
 
+  /**
+   * Modules to enable.
+   */
   protected static $modules = [
     'tripal',
     'tripal_chado',
     'trpcultivate_phenotypes'
   ];
 
+  /**
+   * Configuration
+   * 
+   * @var config_entity
+   */
+  private $config;
+
+  /**
+   * {@inheritdoc}
+   */
   protected function setUp(): void {
     parent::setUp();
-    $this->installConfig(['trpcultivate_phenotypes']);
-
-    $this->service = \Drupal::service('trpcultivate_phenotypes.terms');
-  }
-
-  public function testTermService() {
-    \Drupal::state()->set('is_a_test_environment', TRUE);
-
-    // Class created.
-    $this->assertNotNull($this->service);
     
-    // #Test defineTerms().
-    $define_terms = $this->service->defineTerms();
-    $keys = array_keys($define_terms);
-
-    $this->assertNotNull($define_terms);
-    // Is an array.
-    $is_array = (is_array($define_terms)) ? TRUE : FALSE;
-    $this->assertTrue($is_array);
-
-    // Compare what was defined and the pre-defined terms in the
-    // config settings file.
-    $config = \Drupal::configFactory()->getEditable('trpcultivate_phenotypes.settings');
-    $term_set = $config->get('trpcultivate.default_terms.term_set');
-    foreach($term_set as $id => $terms) {
-      foreach($terms['terms'] as $term) {
-        $this->assertNotNull($term['config_map']);
-      }
-    }
-
-    // #Test loadTerms().
-    // Load Tripal legacy functions in tripal_chado/api directory.
+    // Set test environment.
+    \Drupal::state()->set('is_a_test_environment', TRUE);
+    
+    // Install module configuration.
+    $this->installConfig(['trpcultivate_phenotypes']);
+    $this->config = \Drupal::configFactory()->getEditable('trpcultivate_phenotypes.settings');
+  
+    // Install required dependencies.
     $tripal_chado_path = 'modules/contrib/tripal/tripal_chado/src/api/';
     $tripal_chado_api = [
       'tripal_chado.cv.api.php', 
@@ -75,17 +70,41 @@ class ServiceTermTest extends KernelTestBase {
       closedir($handle);
     }
     
-    // This line will create install schema.
+    // This line will create chado install schema.
     $this->installSchema('tripal_chado', ['chado_installations']);
-    $chado = \Drupal::service('tripal_chado.database');
-    $schema = $chado->getSchemaName();
+ 
+    // Term service.
+    $this->service = \Drupal::service('trpcultivate_phenotypes.terms');
+  }
+
+  public function testTermService() {
+    // Class was created.
+    $this->assertNotNull($this->service);
     
-    $tp = \Drupal::configFactory()->getEditable('tripal_chado.settings');
-    $tp->set('default_schema', $schema);
-    
-    $is_loaded = $this->service->loadTerms($schema);
+    // Test defineTerms().
+    $define_terms = $this->service->defineTerms();
+    $keys = array_keys($define_terms);
+
+    $this->assertNotNull($define_terms);
+    // Is an array.
+    $is_array = (is_array($define_terms)) ? TRUE : FALSE;
+    $this->assertTrue($is_array);
+
+    // Compare what was defined and the pre-defined terms in the
+    // config settings file.
+    $term_set = $this->config->get('trpcultivate.default_terms.term_set');
+    foreach($term_set as $id => $terms) {
+      foreach($terms['terms'] as $term) {
+        $match = (in_array($term['config_map'], $keys)) ? TRUE : FALSE;
+        $this->assertNotNull($term['config_map']);
+        $this->assertTrue($match);
+      }
+    }
+
+    // Test loadTerms().
+    $is_loaded = $this->service->loadTerms('chado');
     $this->assertTrue($is_loaded);
-  
+
     // #Test getTermId().
     foreach($keys as $key) {
       $id = $this->service->getTermId($key);
@@ -107,6 +126,7 @@ class ServiceTermTest extends KernelTestBase {
     }
     
     // Test values matched to what was loaded into the table.
+    $chado = \Drupal::service('tripal_chado.database');
     $all_ids = array_keys($id_to_name);
     $rec = $chado->query(
       'SELECT cvterm_id, name FROM {1:cvterm} WHERE cvterm_id IN(:id[])', 
@@ -141,11 +161,6 @@ class ServiceTermTest extends KernelTestBase {
 
     // Nothing to save.
     $not_saved = $this->service->saveTermConfigValues([]);
-    $this->assertFalse($not_saved);
-
-    // Non-existent key.
-    // This will fail but Tripal Logger will break the test.
-    // $not_saved = $this->service->saveTermConfigValues(['fraud' => 1]);
-    // $this->assertFalse($not_saved);
+    $this->assertFalse($not_saved);    
   }
 }
