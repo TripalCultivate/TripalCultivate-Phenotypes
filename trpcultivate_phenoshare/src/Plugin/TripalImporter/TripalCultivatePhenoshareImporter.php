c6d40fc1173ed54662d4ac4f6407422ad3e83ed3
Reynold Tan <reynoldltan@gmail.com>
2023-08-09 15:48:24 -0600

    Update stage implementation. @TODO: style header notes area and update accordion script

--- trpcultivate_phenoshare/src/Plugin/TripalImporter/TripalCultivatePhenoshareImporter.php
+++ trpcultivate_phenoshare/src/Plugin/TripalImporter/TripalCultivatePhenoshareImporter.php
@@ -27,7 +27,7 @@ use Drupal\tripal_chado\TripalImporter\ChadoImporterBase;
  *   use_button = False,
  *   require_analysis = False,
  *   button_text = "",
- *   file_upload = False,
+ *   file_upload = True,
  *   file_load = False,
  *   file_remote = False,
  *   file_required = False,
@@ -47,21 +47,29 @@ class TripalCultivatePhenoshareImporter extends ChadoImporterBase {
     // Always call the parent form to ensure Chado is handled properly.
     $form = parent::form($form, $form_state);
     
+    // Attach scripts and libraries.
+    $form['#attached']['library'] = [
+      'trpcultivate_phenotypes/trpcultivate-phenotypes-style-accordion',
+      'trpcultivate_phenotypes/trpcultivate-phenotypes-script-accordion'
+    ];
+
     // Reminder to user about expected phenotypes.
     $phenotypes_minder = t('Phenotypic data should be filtered for outliers and mis-entries before
       being uploaded here. Do not upload data that should not be used in the final analysis for a
       scientific article. Furthermore, data should NOT BE AVERAGED across replicates or site-year.');
     \Drupal::messenger()->addWarning($phenotypes_minder);
 
-    // Set stage number.
-    // Describe the stages and help text/guide for this importer.
-    // Stage indicators.
-    $help_text = t('This is a test help text.');
-    $stages = [
-      1 => 'Upload Data File',
-      2 => 'Validate Data',
-      3 => 'Describe and Save Data'
-    ];
+    // Get all methods that implements a stage accordion.
+    // This will become the basis of the stages rendered in accordion layout.
+    $importer_methods = get_class_methods(get_class($this));
+    $stages = [];
+    foreach ($importer_methods as $method) {
+      if (preg_match('/stage([1-9])/', $method, $matches)) {
+        if ($stage_no = $matches[1]) {
+          $stages[ $stage_no ] = $method;
+        } 
+      }
+    }
 
     // Determine the stage.
     $current_stage = 'current_page';
@@ -97,29 +105,150 @@ class TripalCultivatePhenoshareImporter extends ChadoImporterBase {
         'cur_stage' => $stage,
       ],
     ];
-    
-    // Upload stages in accordion layout.
-    $form['stage_accordion'] = [
-      '#type' => 'inline_template',
-      '#theme' => 'theme-accordion',
-      '#weight' => -90,
-      '#data' => [
-        'stages' => $stages,
-        'cur_stage' => $stage,
-      ],
+
+    // Outline the column headers plus description and theme the 
+    // list as an ordered list. Include in this area a link to download
+    // a pre-configured data collection file template.
+    $column_headers = [
+      'Header 1: Header 1 Description',
+      'Header 2: Header 2 Description',
+      'Header 3: Header 3 Description',
+      'Header 4: Header 4 Description',
+      'Header 5: Header 5 Description',
     ];
 
+    $form['importer_notes'] = [
+      '#theme' => 'item_list',
+      '#title' => t('This should be a tab-separated file with the following columns'),
+      '#list_type' => 'ol',
+      '#items' => $column_headers,
+      '#attributes' => ['id' => 'tcp-importer-notes'],
+    ];
+    
+    // Template file expected in module directory/templates/data-file-template.tsv.
+    $form['importer_file_template'] = [
+      '#markup' => '<a href="#">This is a link to a template file</a>'
+    ];
 
-
+    
+    // Render stage accordion callback.
+    foreach($stages as $stage_callback) {
+      // Render Stage Accordion.
+      $this->$stage_callback($form, $form_state);
+    }
 
     return $form;
   }
+
+
+  /// Stage accordion callback.
+
+  /**
+   * Stage 1: Upload data file callback.
+   * 
+   * Subsequent stages in accordion will correspond to a public method titled
+   * stage + stage number (ie. stage1, or stage2). In each method
+   * will define the stage markup using the structure below:
+   *
+   * <div class="tcp-stage">Title</div>
+   * <div>Stage Body/Content</div>
+   * 
+   * Additional field elements will be wrapped using the field wrapper
+   * variable defined in each stage accordion. 
+   * 
+   * @see accordion stlying (css) and behaviours (js).
+   * 
+   * @param $form
+   *   Drupal form object.
+   * @param $form_state
+   *   Drupal form state object.
+   */
+  public function stage1(&$form, $form_state) {
+    // Describe stage.
+    $stage = [
+      'stage#' => 1,
+      'title' => 'Upload Data File'
+    ];
+    
+    $fld_wrapper = 'accordion_stage' . $stage['stage#'];
+    $form[ $fld_wrapper ] = [
+      '#prefix' => t('<div class="tcp-stage">Stage @stage#: @title</div><div>', 
+        ['@stage#' => $stage['stage#'], '@title' => $stage['title']]),
+      '#suffix' => '</div>'
+    ];
+
+    // Other relevant fields here.
+    
+    // Apply field stage field wrapper to file upload element.
+    // For the file upload field to conform to the accordion layout,
+    // this override script must be performed.
+    $file_upload = $form['file'];
+    $form[ $fld_wrapper ]['file'] = $file_upload;
+    // Omit old copy so there would not be duplicate file
+    // element in the upload data file stage.
+    $form['file'] = [];
+    ///
+
+    // Other relevant fields here.
+  }
+
+  /**
+   * Stage 2: Validate data. 
+   */
+  public function stage2(&$form, $form_state) {
+    // Describe stage.
+    $stage = [
+      'stage#' => 2,
+      'title' => 'Validate Data'
+    ];
+    
+    $fld_wrapper = 'accordion_stage' . $stage['stage#'];
+    $form[ $fld_wrapper ] = [
+      '#prefix' => t('<div class="tcp-stage">Stage @stage#: @title</div><div>', 
+        ['@stage#' => $stage['stage#'], '@title' => $stage['title']]),
+      '#suffix' => '</div>'
+    ];
+    
+    // Other relevant fields here.
+    $form[ $fld_wrapper ]['field_elements'] = [
+      '#markup' => 'Stage 2 field elements here'
+    ];
+  }
+
+  /**
+   * Stage 3: Describe and save data. 
+   */
+  public function stage3(&$form, $form_state) {
+    // Describe stage.
+    $stage = [
+      'stage#' => 3,
+      'title' => 'Describe and Save Data'
+    ];
+    
+    $fld_wrapper = 'accordion_stage' . $stage['stage#'];
+    $form[ $fld_wrapper ] = [
+      '#prefix' => t('<div class="tcp-stage">Stage @stage#: @title</div><div>', 
+        ['@stage#' => $stage['stage#'], '@title' => $stage['title']]),
+      '#suffix' => '</div>'
+    ];
+    
+    // Other relevant fields here.
+    $form[ $fld_wrapper ]['field_elements'] = [
+      '#markup' => 'Stage 3 field elements here'
+    ];
+  }
+
+  // End stage accordion callback.
+
+  ///
+
 
   /**
    * {@inheritdoc}
    */
   public function formSubmit($form, &$form_state) {
     $form_state->setRebuild(TRUE);
+    dpm($form_state);
   }
 
   /**
--- trpcultivate_phenotypes/js/trpcultivate-phenotypes-script-accordion.js
+++ trpcultivate_phenotypes/js/trpcultivate-phenotypes-script-accordion.js
@@ -1,11 +1,22 @@
 /**
- * @file
- * Manage events in stage accordion.
- */
+* @file
+* Manage events in stage accordion.
+*/
 
-(function($) {
-  Drupal.behaviors.stageAccordion = {
-    attach: function (context, settings) {
+
+(function ($, Drupal, drupalSettings) {
+  var initialized;
+ 
+ 
+  /**
+   * Initialize stage accordion.
+   */
+  function initAccordion() {
+    if (!initialized) {
+      initialized = true;
+      ///
+ 
+ 
       // Stage accordion element class object.
       // @see css/style-accordion.
       var tcpAccordion = {};
@@ -18,23 +29,47 @@
       // Class accordion content panel expanded.
       // Active content panel.
       tcpAccordion.on  = 'tcp-stage-accordion-content-on';
-      
+     
       // Reference all accordion title elements.
-      var tcpAccordionTitle = document.getElementsByClassName(tcpAccordion.title); 
+      var tcpAccordionTitle = document.getElementsByClassName(tcpAccordion.title);
       // Add click event listener.
       for(var i = 0; i < tcpAccordionTitle.length; i++) {
         tcpAccordionTitle[i].addEventListener('click', function() {
-          // Inspect the current state of the content panel to know
+          // Reset any previously activated stage to only show
+          // one stage at a time.
+          var active = document.getElementsByClassName(tcpAccordion.active);
+          var panels = document.getElementsByClassName(tcpAccordion.on);
+          if (active.length > 0) {
+            for (j = 0; j < active.length; j++) {
+              active[j].classList.remove(tcpAccordion.active);
+              panels[j].classList.replace(tcpAccordion.on, tcpAccordion.off);
+            }
+          }
+ 
+ 
+          // Content panel of a stage.
+          var tcpAccordionContent = this.nextElementSibling;
+ 
+ 
+          // Inspect the current state of the stage title to know
           // if section should expanded or collapsed.
-          if(this.classList.contains(tcpAccordion.active)) {
+          if(!this.classList.contains(tcpAccordion.active)) {
             // It is in collapsed state. Proceed to expand.
             this.classList.add(tcpAccordion.active);
+            tcpAccordionContent.classList.replace(tcpAccordion.off, tcpAccordion.on);
           }
           else {
             // It is in expanded state. Proceed to collapse.
             this.classList.remove(tcpAccordion.active);
+            tcpAccordionContent.classList.replace(tcpAccordion.on, tcpAccordion.off);
           }
         });
       }
-    ///
-}}}(jQuery));
+    }
+  }
+   Drupal.behaviors.stageAccordion = {
+    attach: function(context, settings) {
+      initAccordion();
+    }
+  }
+ } (jQuery, Drupal, drupalSettings)); 
