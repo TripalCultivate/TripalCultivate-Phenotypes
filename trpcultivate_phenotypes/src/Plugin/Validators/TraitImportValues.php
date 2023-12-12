7e866c80156e4f52ab224c40853933fd27a6be0a
Reynold Tan <reynoldltan@gmail.com>
2023-12-12 15:44:37 -0600

    Revises trait importer

--- trpcultivate_phenotypes/src/Plugin/Validators/TraitImportValues.php
+++ trpcultivate_phenotypes/src/Plugin/Validators/TraitImportValues.php
@@ -52,6 +52,22 @@ class TraitImportValues extends TripalCultivatePhenotypesValidatorBase implement
    *   NOTE: keep track of the line no to point user exactly which line failed.
    */
   public function validate() {
+    // Keys to indicate and EMPTY value and DUPLICATE trait name in the same genus.
+    $error_types = [
+      'empty' => [
+        'key' => '#EMPTY', 
+        'info' => 'Empty values',
+      ],
+      'duplicate' => [
+        'key' => '#DUPLICATE',
+        'info' => 'Duplicate traits',
+      ],
+      'unexpected' => [
+        'key' => '#UNEXPECTED',
+        'info' => 'Unexpected Type (Qualitative or Quantitative only)',
+      ] 
+    ];
+
     // Validate ...
     $validator_status = [
       'title' => 'Column Headers have Values',
@@ -70,6 +86,7 @@ class TraitImportValues extends TripalCultivatePhenotypesValidatorBase implement
     // Values:
     //   - Ensure that each column header has a value.
     //   - Column type value is either Quantitative or Qualitative.
+    //   - No duplicate trait name + method short name and unit in the same Genus (data file).
     
     // Load file object.
     $file = File::load($this->file_id);
@@ -79,8 +96,16 @@ class TraitImportValues extends TripalCultivatePhenotypesValidatorBase implement
     
     // Line counter.
     $line_no = 0;
-    // Array to hold all failed line.
+    // Line check - line that has value and is not empty.
+    $line_check = 0;
+    // Array to hold all failed line, sorted by error type.
     $failed_rows = [];
+    // Count each time a trait is process. This will be used
+    // to check for any duplicate trait name in the same genus.
+    $trait_count = [];
+    // Header by keys.
+    $header_key = array_flip($this->column_headers);
+
 
     // Begin column and row validation.
     while(!feof($handle)) {
@@ -88,25 +113,75 @@ class TraitImportValues extends TripalCultivatePhenotypesValidatorBase implement
       $line = fgets($handle);
 
       if ($line_no > 0 && !empty(trim($line))) {
+        $line_check++;
+
         // Header row is index 0.
         // Continue to data row. No need to use the headers in the file
         // instead use the headers property from the importer.  
      
         // Data rows.
         // On wards, line number starts at #1.
-
+        
         // Line split into individual data point.
         $data_columns = str_getcsv($line, "\t");
 
         // Sanitize every data in rows and columns.
         $data = array_map(function($col) { return isset($col) ? trim(str_replace(['"','\''], '', $col)) : ''; }, $data_columns);
-               
-        foreach($data as $i => $value) {
-          if (empty($value) && isset($this->column_headers[ $i ])) {
-            $failed_rows[ $line_no ][] = $this->column_headers[ $i ]; 
+        
+        // Validate.
+        // Duplicate names.
+        $unique_cols = '';
+        if (isset($data[ $header_key['Trait Name'] ]) 
+            && isset($data[ $header_key['Method Short Name'] ]) 
+            && isset($data[ $header_key['Unit'] ])) {
+
+          $unique_cols = $data[ $header_key['Trait Name'] ] . ' - ' 
+            . $data[ $header_key['Method Short Name'] ] . ' - ' 
+            . $data[ $header_key['Unit'] ];
+          
+          if (!in_array($unique_cols, array_keys($trait_count))) {
+            $trait_count[ $unique_cols ] = [];
           }
         }
 
+        // Record the line number trait names (combination) is used.
+        // No need to track the column header as it is the combination of 
+        // Trait Name + Method Short Name and Unit.
+        if ($unique_cols) {
+          $trait_count[ $unique_cols ][] = $line_no;
+        }
+
+        if (count($trait_count[ $unique_cols ]) > 1) {
+          // Reference all duplicates.
+          foreach($trait_count[ $unique_cols ] as $duplicate_lines) {
+            $failed_rows[ $error_types['duplicate']['key'] ][ $duplicate_lines ][] = '';
+          }
+        }
+
+        // Empty and unexpected value.
+        foreach($this->column_headers as $i => $header) {
+          if (!isset($data[ $i ]) || empty($data[ $i ])) {
+            // Empty values.
+            // Track both line number and which column header.
+            $failed_rows[ $error_types['empty']['key'] ][ $line_no ][] = $header;
+          }
+          else {
+            // Has value. Check for unexpected values.
+            if ($header == 'Type') {
+              $type = strtolower($data[ $i ]);
+              
+              if (!in_array($type, ['qualitative', 'quantitative'])) {
+                // Unexpected value in Type column.
+                // No need to track the column header as it is the Type column.
+                $failed_rows[ $error_types['unexpected']['key'] ][ $line_no ][] = '';
+              }
+            }
+          }
+        }
+
+
+        
+        // Reset data.
         unset($data);
       }
 
@@ -116,20 +191,41 @@ class TraitImportValues extends TripalCultivatePhenotypesValidatorBase implement
     // Close the file.
     fclose($handle);
 
-    // Array to hold all failed line.
-    if (count($failed_rows) > 0) {
-      // Construct failed lines to say: line number - list of headers that has empty value.
-      $line = 'Empty values found in the following line number and column header: ';
-      foreach($failed_rows as $line_no => $headers) {
-        $line .= '@ line #' . $line_no . ' Column(s): ' . implode(', ', $headers) . ' ';
-      }
-
+    // It seems the file has no data rows.
+    if (!$line_check) {
       // Report validation result.
       $validator_status = [
         'title' => 'Column Headers have Values',
         'status' => 'fail',
-        'details' => $line
-      ];  
+        'details' => 'Data file has no data rows to process. Please upload a file and try again.'
+      ];
+
+      return $validator_status;
+    }
+    
+    // Prepare summary report.
+    if (count($failed_rows) > 0) {
+      $line = [];
+      
+      // Each error type, construct error message.
+      foreach($error_types as $type => $error) {
+        if (isset($failed_rows[ $error_types[ $type ]['key'] ])) {
+          // Error type.
+          $line[ $error_types[ $type ]['key'] ] = $error['info'];
+
+          // Error line number and column header.
+          foreach($failed_rows[ $error_types[ $type ]['key'] ] as $line_no => $header) {
+            // @TODO: Construct line message.
+          }
+        }
+      }
+      
+      // Report validation result.
+      $validator_status = [
+        'title' => 'Column Headers have Values',
+        'status' => 'fail',
+        'details' => $line,
+      ];
     }
 
     return $validator_status;
