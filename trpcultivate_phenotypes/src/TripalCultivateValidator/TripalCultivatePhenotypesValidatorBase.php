7cdf2dd271c8e61848da11963081f29e7e5ad761
Lacey-Anne Sanderson <lacey.sanderson@usask.ca>
2024-06-26 16:48:55 -0600

    Update interface and base with new validateMethods and return value.

--- trpcultivate_phenotypes/src/TripalCultivateValidator/TripalCultivatePhenotypesValidatorBase.php
+++ trpcultivate_phenotypes/src/TripalCultivateValidator/TripalCultivatePhenotypesValidatorBase.php
@@ -5,34 +5,85 @@ namespace Drupal\trpcultivate_phenotypes\TripalCultivateValidator;
 use Drupal\Component\Plugin\PluginBase;
 
 abstract class TripalCultivatePhenotypesValidatorBase extends PluginBase implements TripalCultivatePhenotypesValidatorInterface {
+
+  /**
+   * Get validator plugin validator_name definition annotation value.
+   *
+   * @return string
+   *   The validator plugin name annotation definition value.
+   */
+  public function getValidatorName() {
+    return $this->pluginDefinition['validator_name'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateMetadata(array $form_values) {
+    $plugin_name = $this->getValidatorName();
+    throw \Exception("Method validateMetadata from base class called for $plugin_name. If this plugin wants to support this type of validation then they need to override it.");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateFile(string $filename, int $fid) {
+    $plugin_name = $this->getValidatorName();
+    throw \Exception("Method validateFile from base class called for $plugin_name. If this plugin wants to support this type of validation then they need to override it.");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateRow(array $form_values) {
+    $plugin_name = $this->getValidatorName();
+    throw \Exception("Method validateRow from base class called for $plugin_name. If this plugin wants to support this type of validation then they need to override it.");
+  }
+
+  /**
+   * {@inheritdoc}
+   * @deprecated Remove in issue #91
+   */
+  public function validate() {
+    $plugin_name = $this->getValidatorName();
+    throw \Exception("Method validate from base class called for $plugin_name. This method is being deprecated and should be upgraded to validateMetadata(), validateFile() or validateRow().");
+  }
+
   /**
    * Project name/title.
+   * @deprecated Remove in issue #91
    */
   public $project;
 
   /**
    * Genus.
+   * @deprecated Remove in issue #91
    */
   public $genus;
 
   /**
    * Drupal File ID Number.
+   * @deprecated Remove in issue #91
    */
   public $file_id;
 
   /**
    * Required column headers as defined in the importer.
+   * @deprecated Remove in issue #91
    */
   public $column_headers;
 
   /**
    * Skip flag, indicate validator not to execute validation logic and
    * set the validator as upcoming or todo.
+   * @deprecated Remove in issue #91
    */
   public $skip;
 
   /**
    * Load phenotypic data upload assets to validated.
+   *
+   * @deprecated Remove in issue #91
    *
    * @param $project
    *   String, Project name/title - chado.project: name.
@@ -63,17 +114,9 @@ abstract class TripalCultivatePhenotypesValidatorBase extends PluginBase impleme
   }
 
   /**
-   * Get validator plugin validator_name definition annotation value.
-   *
-   * @return string
-   *   The validator plugin name annotation definition value.
-   */
-  public function getValidatorName() {
-    return $this->pluginDefinition['validator_name'];
-  }
-
-  /**
    * Get validator plugin validator_scope definition annotation value.
+   *
+   * @deprecated Remove in issue #91
    *
    * @return string
    *   The validator plugin scope annotation definition value.
--- trpcultivate_phenotypes/src/TripalCultivateValidator/TripalCultivatePhenotypesValidatorInterface.php
+++ trpcultivate_phenotypes/src/TripalCultivateValidator/TripalCultivatePhenotypesValidatorInterface.php
@@ -8,6 +8,7 @@ use Drupal\Component\Plugin\PluginInspectionInterface;
  * Defines an interface for data validator plugin.
  */
 interface TripalCultivatePhenotypesValidatorInterface extends PluginInspectionInterface {
+
   /**
    * Return the name of the validator.
    *
@@ -16,7 +17,86 @@ interface TripalCultivatePhenotypesValidatorInterface extends PluginInspectionIn
   public function getValidatorName();
 
   /**
+   * Validates the metadata associated with an importer.
+   *
+   * This should never validate the file even though it will likely be passed in
+   * with the other form values.
+   *
+   * @param array $form_values
+   *  An array of values from the submitted form where each key maps to a form
+   *  element and the value is what the user entered.
+   *
+   * @return array
+   *  An array of information about the validity of the data passed in.
+   *  The supported keys are:
+   *  - 'case': a developer code describing the case triggered
+   *      (i.e. no record in chado matching project name). If the data is
+   *      is valid then this is not required but could be 'data verified'.
+   *  - 'valid': a boolean indicating the data is valid (TRUE) or not (FALSE)
+   *  - 'failedIems': an array of the items that failed validation. For example,
+   *      if this validator validates a number of indicies are not empty then
+   *      this will be an array of indices that were empty.
+   *  The old style keys we are deprecating are:
+   *  - title: the title of the validation (shown both when passes or fails).
+   *  - details: string describing the failure to users with failed items embedded.
+   *  - status: one of 'pass' or 'fail'
+   */
+  public function validateMetadata(array $form_values);
+
+  /**
+   * Validates the file associated with an importer.
+   *
+   * This should validate the file object (e.g. it exists, is readable) but
+   * should not validate the contents in any way.
+   *
+   * @param array $filename
+   *  The full path and filename with extension of the file to validate.
+   *
+   * @return array
+   *  An array of information about the validity of the data passed in.
+   *  The supported keys are:
+   *  - 'case': a developer code describing the case triggered
+   *      (i.e. no record in chado matching project name). If the data is
+   *      is valid then this is not required but could be 'data verified'.
+   *  - 'valid': a boolean indicating the data is valid (TRUE) or not (FALSE)
+   *  - 'failedIems': an array of the items that failed validation. For example,
+   *      if this validator validates a number of indicies are not empty then
+   *      this will be an array of indices that were empty.
+   *  The old style keys we are deprecating are:
+   *  - title: the title of the validation (shown both when passes or fails).
+   *  - details: string describing the failure to users with failed items embedded.
+   *  - status: one of 'pass' or 'fail'
+   */
+  public function validateFile(string $filename, int $fid);
+
+  /**
+   * Validates rows within the data file submitted to an importer.
+   *
+   * @param array $row
+   *  An array of values from a single row/line in the file where each element
+   *  is a single column.
+   *
+   * @return array
+   *  An array of information about the validity of the data passed in.
+   *  The supported keys are:
+   *  - 'case': a developer code describing the case triggered
+   *      (i.e. no record in chado matching project name). If the data is
+   *      is valid then this is not required but could be 'data verified'.
+   *  - 'valid': a boolean indicating the data is valid (TRUE) or not (FALSE)
+   *  - 'failedIems': an array of the items that failed validation. For example,
+   *      if this validator validates a number of indicies are not empty then
+   *      this will be an array of indices that were empty.
+   *  The old style keys we are deprecating are:
+   *  - title: the title of the validation (shown both when passes or fails).
+   *  - details: string describing the failure to users with failed items embedded.
+   *  - status: one of 'pass' or 'fail'
+   */
+  public function validateRow(array $form_values);
+
+  /**
    * Return the scope of the validator.
+   *
+   * @deprecated Remove in issue #91
    *
    * @return string.
    */
@@ -25,6 +105,8 @@ interface TripalCultivatePhenotypesValidatorInterface extends PluginInspectionIn
   /**
    * Load data file import assets Project title, Genus and Data File Id
    * as entered in the Importer form.
+   *
+   * @deprecated Remove in issue #91
    *
    * @param $project
    *   String, Project name/title - chado.project: name.
@@ -45,6 +127,8 @@ interface TripalCultivatePhenotypesValidatorInterface extends PluginInspectionIn
 
   /**
    * Validate items in the phenotypic data upload assets.
+   *
+   * @deprecated Remove in issue #91
    *
    * @return array
    *   An associative array with the following keys.
