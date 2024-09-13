342b7f7bf62e0dfc46bc6806afa6248d8caed184
Reynold Tan <reynoldltan@gmail.com>
2024-09-13 16:05:35 -0600

    Revised headers validator

---
+++ trpcultivate_phenotypes/src/Plugin/Validators/ValidHeaders.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * @file
+ * Contains validator plugin definition.
+ */
+
+namespace Drupal\trpcultivate_phenotypes\Plugin\Validators;
+
+use Drupal\trpcultivate_phenotypes\TripalCultivateValidator\TripalCultivatePhenotypesValidatorBase;
+use Drupal\trpcultivate_phenotypes\TripalCultivateValidator\ValidatorTraits\Headers;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Validate that the header row is not empty and that all expected column headers exist.
+ *
+ * @TripalCultivatePhenotypesValidator(
+ *   id = "valid_headers",
+ *   validator_name = @Translation("Header Row Validator"),
+ *   input_types = {"header-row"}
+ * )
+ */
+class ValidHeaders extends TripalCultivatePhenotypesValidatorBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * This validator requires the following validator traits:
+   * - Headers - getHeaders: get all headers.
+   */
+  use Headers;
+
+  /**
+   * Constructor.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+    );
+  }
+
+  /**
+   * Validate the header row.
+   * Checks include:
+   *  - Each header value is a non-empty string.
+   *  - No header is missing.
+   *  - The order of headers defined by the Importer should match the order of headers array.
+   *
+   * @param array $headers
+   *   An array of headers created by splitting the first line of the data file into separate values.
+   *   The index of the header represents the order they appear in the line.
+   *
+   * @return array
+   *   An associative array with the following keys.
+   *     - case: a developer focused string describing the case checked.
+   *     - valid: either TRUE or FALSE depending on if the header is valid or not.
+   *     - failedItems: the failed headers. This will be an empty array if the header was valid.
+   */
+  public function validateRow($headers) {
+
+    // Validator response values for a valid raw row.
+    $case = 'Headers exist and match expected headers';
+    $valid = TRUE;
+    $failed_items = [];
+
+    // Check that the headers array is not empty.
+    if ($headers) {
+
+      // Check that no headers are missing by verifying that all expected headers are present.
+      // Get all the headers defined by the importer regardless of type.
+      $expected_headers = $this->getHeaders();
+      $expected_header_names = array_values($expected_headers);
+
+      $missing_headers = array_filter($expected_header_names, function($names) use($headers) {
+        return (!in_array($names, $headers));
+      });
+
+      if (!$missing_headers) {
+  
+        // Check that the sequence of headers specified by the Importer is the same as the order
+        // of headers as in the headers array.
+        $not_in_order = [];
+
+        foreach($expected_headers as $index => $header) {
+          if (isset($headers[ $index ]) && $headers[ $index ] != $header) {
+            // Check that in this index in the expected headers, the header name is the same as the name
+            // in the headers array in the same position or index.
+            
+            // Save only the header that does not match.
+            array_push($not_in_order, $header);
+          }
+        }
+
+        if ($not_in_order) {
+          // Headers not in the correct order.
+          $not_in_order = implode(', ', $not_in_order);
+
+          $case = 'Headers not in the correct order';
+          $valid = FALSE;
+          $failed_items = ['headers' => $not_in_order];
+        }
+      }
+      else {
+        // Expected header/s are missing.
+        $missing_headers = implode(', ', $missing_headers);
+
+        $case = 'Missing expected headers';
+        $valid = FALSE;
+        $failed_items = ['headers' => $missing_headers];
+      }
+    }
+    else {
+      // Headers array is an empty array.
+      $case = 'Header row is an empty value';
+      $valid = FALSE;
+      $failed_items = ['headers' => 'headers array is an empty array'];
+    }
+
+    return [
+      'case' => $case,
+      'valid' => $valid,
+      'failedItems' => $failed_items
+    ];
+  }
+}
---
+++ trpcultivate_phenotypes/tests/src/Kernel/Validators/ValidHeadersTest.php
@@ -0,0 +1,176 @@
+<?php
+
+/**
+ * @file
+ * Kernel tests for validator plugins specific to validating headers.
+ */
+
+namespace Drupal\Tests\trpcultivate_phenotypes\Kernel\Validators;
+
+use Drupal\Tests\tripal_chado\Kernel\ChadoTestKernelBase;
+use Drupal\Tests\trpcultivate_phenotypes\Traits\PhenotypeImporterTestTrait;
+use Drupal\file\Entity\File;
+
+ /**
+  * Tests Tripal Cultivate Phenotypes Headers Validator Plugin.
+  *
+  * @group trpcultivate_phenotypes
+  * @group validators
+  */
+class ValidHeadersTest extends ChadoTestKernelBase {
+
+  use PhenotypeImporterTestTrait;
+
+  /**
+   * An instance of the data file validator.
+   *
+   * @var object
+   */
+  protected $validator_instance;
+
+  /**
+   * Modules to enable.
+   */
+  protected static $modules = [
+    'file',
+    'user',
+    'tripal',
+    'tripal_chado',
+    'trpcultivate_phenotypes'
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // Set test environment.
+    \Drupal::state()->set('is_a_test_environment', TRUE);
+
+    // Create a plugin instance for this validator
+    $validator_id = 'valid_headers';
+    $this->validator_instance = \Drupal::service('plugin.manager.trpcultivate_validator')
+      ->createInstance($validator_id);
+
+    // Set the importer headers.
+    $this->validator_instance->setHeaders([
+      [
+        'name' => 'Header 0',
+        'type' => 'required'
+      ],
+      [
+        'name' => 'Header 1',
+        'type' => 'required'
+      ],
+      [
+        'name' => 'Header 2',
+        'type' => 'required'
+      ],
+      [
+        'name' => 'Header 3',
+        'type' => 'optional'
+      ],
+      [
+        'name' => 'Header 4',
+        'type' => 'optional'
+      ],
+      [
+        'name' => 'Header 5',
+        'type' => 'required'
+      ]
+    ]);
+  }
+
+  /**
+   * Data provider: provides test headers input.
+   *
+   * @return array
+   *   Each scenario/element is an array with the following values.
+   *
+   *   - A string, human-readable short description of the test scenario.
+   *   - Headers input array.
+   *   - Expected validation result.
+   */
+  public function provideHeadersToHeadersValidator() {
+  
+    return [
+      // #0: The headers input is an empty array.
+      [
+        'empty headers',
+        [],
+        [
+          'case' => 'Header row is an empty value',
+          'valid' => FALSE,
+          'failedItems' => ['headers' => 'headers array is an empty array']
+        ]
+      ],
+
+      // #1: Missing some headers.
+      [
+        'missing header',
+        [
+          'Header 0',
+          'Header 1',
+          'Header 2',
+          'Header 4',
+        ],
+        [
+          'case' => 'Missing expected headers',
+          'valid' => FALSE,
+          'failedItems' => ['headers' => 'Header 5, Header 3']
+        ]
+      ],
+
+      // #2: Header not in the right order.
+      [
+        'not in the order',
+        [
+          'Header 0',
+          'Header 2',
+          'Header 1',
+          'Header 3',
+          'Header 4',
+          'Header 5'
+        ],
+        [
+          'case' => 'Headers not in the correct order',
+          'valid' => FALSE,
+          'failedItems' => ['headers' => 'Header 1, Header 2']
+        ]
+      ],
+
+      // #3: A valid header (list and order).
+      [
+        'valid headers',
+        [
+          'Header 0',
+          'Header 1',
+          'Header 2',
+          'Header 3',
+          'Header 4',
+          'Header 5'
+        ],
+        [
+          'case' => 'Headers exist and match expected headers',
+          'valid' => TRUE,
+          'failedItems' => []
+        ]
+      ]
+    ];
+  }
+
+  /**
+   * Test headers validator.
+   *
+   * @dataProvider provideHeadersToHeadersValidator
+   */
+  public function testHeaders($scenario, $headers_input, $expected) {
+    $validation_status = $this->validator_instance->validateRow($headers_input);
+  
+    foreach($validation_status as $key => $value) {
+      $this->assertEquals($value, $expected[ $key ],
+        'The validation status key: ' . $key . ' does not match the same key in the expected status of scenario: ' . $scenario);
+    }
+  }
+}
