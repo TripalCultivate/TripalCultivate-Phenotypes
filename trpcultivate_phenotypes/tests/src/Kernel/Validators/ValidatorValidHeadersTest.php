951c7e5c47d84c49841be37b0709427bd76a6f40
RLT <reynoldltan@gmail.com>
2024-10-09 14:11:43 -0600

    Revisions to headers validator

--- trpcultivate_phenotypes/tests/src/Kernel/Validators/ValidatorValidHeadersTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/Validators/ValidatorValidHeadersTest.php
@@ -308,7 +308,34 @@ class ValidatorValidHeadersTest extends ChadoTestKernelBase {
         ]
       ],
 
-      // #8: A valid header with string index values.
+      // #8: A valid header with the header count is greater than the expected number and strict is false.
+      [
+        'extra headers',
+        [
+          'Header 0',
+          'Header 1',
+          'Header 2',
+          'Header 3',
+          'Header 4',
+          'Header 5',
+          'Header 6',
+          'Header 7',
+          'Header 8',
+          'Header 9',
+          'Header 10'
+        ],
+        [
+          'case' => 'Headers exist and match expected headers',
+          'valid' => TRUE,
+          'failedItems' => []
+        ],
+        [
+          'number_of_columns' => 6,
+          'strict' => FALSE
+        ]
+      ],
+
+      // #9: A valid header with string index values.
       [
         'string index valid',
         [
@@ -330,7 +357,8 @@ class ValidatorValidHeadersTest extends ChadoTestKernelBase {
         ]
       ],
 
-      // #9: A valid header (list and order).
+
+      // #10: A valid header (list and order).
       [
         'valid headers',
         [
@@ -367,6 +395,22 @@ class ValidatorValidHeadersTest extends ChadoTestKernelBase {
     foreach($validation_status as $key => $value) {
       $this->assertEquals($value, $expected[ $key ],
         'The validation status key: ' . $key . ' does not match the same key in the expected status of scenario: ' . $scenario);
+    }
+
+    // Check that the header input array is the same as the failed item in scenario where
+    // header input array is provided.
+    if ($headers_input) {
+      // Reset the headers input if the headers provided is valid.
+      $headers_input = ($validation_status['valid']) ? [] : $headers_input;
+
+      // Check both arrays headers and failedItems are the same in terms of items and order.
+      foreach($headers_input as $i => $header) {
+        $this->assertEquals(
+          $validation_status['failedItems'][ $i ],
+          $header,
+          'A header in the header input array is in the wrong order in the failed items array in scenario: ' . $scenario
+        );
+      }
     }
   }
 }
