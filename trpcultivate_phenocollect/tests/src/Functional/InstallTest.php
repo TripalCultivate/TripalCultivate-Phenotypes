19a0c3d57094b764e98e7e8868ced4a528800a22
RLT <reynoldltan@gmail.com>
2025-01-24 07:24:25 -0600

    Fixes failed install test

--- trpcultivate_phenocollect/tests/src/Functional/InstallTest.php
+++ trpcultivate_phenocollect/tests/src/Functional/InstallTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\trpcultivate_phenocollect\Functional;
 
+use Drupal\Core\Routing\RouteMatch;
 use Drupal\Core\Url;
 use Drupal\Tests\tripal_chado\Functional\ChadoTestBrowserBase;
 
