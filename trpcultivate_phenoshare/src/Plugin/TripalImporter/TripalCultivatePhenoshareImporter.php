b1237a1e4a1e77e40a644afe3c774f3de54d88d5
RLT <reynoldltan@gmail.com>
2025-02-04 15:59:59 -0600

    Update trpcultivate_phenoshare/src/Plugin/TripalImporter/TripalCultivatePhenoshareImporter.php
    Co-authored-by: Carolyn T Caron <carolyn.caron@usask.ca>

--- trpcultivate_phenoshare/src/Plugin/TripalImporter/TripalCultivatePhenoshareImporter.php
+++ trpcultivate_phenoshare/src/Plugin/TripalImporter/TripalCultivatePhenoshareImporter.php
@@ -704,8 +704,8 @@ class TripalCultivatePhenoshareImporter extends ChadoImporterBase implements Con
           }
 
           if ($submit_form === FALSE) {
-            // Provide a general error message indicating that input values and/or the
-            // data file may contain one or more errors.
+            // Provide a general error message indicating that input values 
+            // and/or the data file may contain one or more errors.
             $this->service_Messenger
               ->addError($this->t('Your file import was not successful. Please check the Validation Result Window for errors and try again.'));
 
