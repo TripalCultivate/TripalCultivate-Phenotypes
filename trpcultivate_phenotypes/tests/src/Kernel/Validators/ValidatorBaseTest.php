6de9278a5799f1f765b13228a37aa5d35723dd61
RLT <reynoldltan@gmail.com>
2025-02-28 10:04:55 -0600

    Fixes failed test in ValidatorBaseTest

--- trpcultivate_phenotypes/tests/src/Kernel/Validators/ValidatorBaseTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/Validators/ValidatorBaseTest.php
@@ -70,7 +70,7 @@ class ValidatorBaseTest extends ChadoTestKernelBase {
     $this->container->set('tripal_chado.database', $this->chado_connection);
 
     // Set plugin manager service.
-    $this->plugin_manager = \Drupal::service('plugin.manager.trpcultivate_validator');
+    $this->plugin_manager = \Drupal::service('plugin.manager.trpcultivate_phenotypes_validator');
   }
 
   /**
