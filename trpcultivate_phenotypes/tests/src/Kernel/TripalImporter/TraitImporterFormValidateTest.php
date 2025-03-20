c28731851ee013150f9541952eed043e561142f9
RLT <reynoldltan@gmail.com>
2025-03-20 16:27:59 -0600

    Provide fixturepath in TraitImporterFormValidateTest

--- trpcultivate_phenotypes/tests/src/Kernel/TripalImporter/TraitImporterFormValidateTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/TripalImporter/TraitImporterFormValidateTest.php
@@ -4,6 +4,7 @@ namespace Drupal\Tests\trpcultivate_phenotypes\Kernel\TripalImporter;
 
 use Drupal\Core\Form\FormState;
 use Drupal\Tests\tripal_chado\Kernel\ChadoTestKernelBase;
+use Drupal\Tests\trpcultivate\Traits\TripalCultivateImporterTestTrait;
 use Drupal\Tests\trpcultivate_phenotypes\Traits\PhenotypeImporterTestTrait;
 use Drupal\Tests\user\Traits\UserCreationTrait;
 use Drupal\tripal\Services\TripalLogger;
@@ -17,6 +18,7 @@ use Drupal\tripal_chado\Database\ChadoConnection;
 class TraitImporterFormValidateTest extends ChadoTestKernelBase {
 
   use UserCreationTrait;
+  use TripalCultivateImporterTestTrait;
   use PhenotypeImporterTestTrait;
 
   /**
@@ -95,6 +97,13 @@ class TraitImporterFormValidateTest extends ChadoTestKernelBase {
   ];
 
   /**
+   * The path to tripalcultivate_phenotypes module.
+   *
+   * @var string
+   */
+  private $module_path;
+
+  /**
    * {@inheritdoc}
    */
   protected function setUp(): void {
@@ -133,6 +142,10 @@ class TraitImporterFormValidateTest extends ChadoTestKernelBase {
         return NULL;
       });
     $container->set('tripal.logger', $mock_logger);
+
+    $this->module_path = $this->container->get('module_handler')
+      ->getModule('trpcultivate_phenotypes')
+      ->getPath();
   }
 
   /**
@@ -427,7 +440,10 @@ class TraitImporterFormValidateTest extends ChadoTestKernelBase {
     // Create a file to upload.
     $file = $this->createTestFile([
       'filename' => $filename,
-      'content' => ['file' => 'TraitImporterFiles/' . $filename],
+      'content' => [
+        'file' => $filename,
+        'fixturepath' => $this->module_path . '/tests/src/Fixtures/TraitImporterFiles/',
+      ],
     ]);
 
     // Setup the form_state.
