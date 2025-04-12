df07a58044c314c0e8e81516ab29474ec6e970df
RLT <reynoldltan@gmail.com>
2025-04-11 20:11:44 -0600

    Revisions to backup form

--- trpcultivate_phenotypes/src/Form/PhenodataBackupForm.php
+++ trpcultivate_phenotypes/src/Form/PhenodataBackupForm.php
@@ -9,7 +9,6 @@ use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
-use Drupal\tripal\TripalImporter\PluginManagers\TripalImporterManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\tripal_chado\Controller\ChadoProjectAutocompleteController;
 
@@ -17,6 +16,13 @@ use Drupal\tripal_chado\Controller\ChadoProjectAutocompleteController;
  * Phenotypic Data Backup form.
  */
 final class PhenodataBackupForm extends EntityForm {
+
+  /**
+   * File types supported.
+   */
+  private const array BACKUP_FILE_TYPES = [
+    'tsv',
+  ];
 
   /**
    * Entity type manager service.
@@ -60,21 +66,17 @@ final class PhenodataBackupForm extends EntityForm {
    *   Entity Type manager service.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   Configuration factory service.
-   * @param \Drupal\tripal\TripalImporter\PluginManagers\TripalImporterManager $tripal_importer_manager
-   *   Tripal importer plugin manager.
    * @param \Drupal\Core\Session\AccountInterface $user
    *   Drupal users.
    */
   public function __construct(
     EntityTypeManagerInterface $entity_type_manager,
     ConfigFactoryInterface $config_factory,
-    TripalImporterManager $tripal_importer_manager,
     AccountInterface $user,
   ) {
 
     $this->service_EntityTypeManager = $entity_type_manager;
     $this->service_ConfigFactory = $config_factory;
-    $this->tripal_importer = $tripal_importer_manager;
     $this->user = $user;
   }
 
@@ -86,7 +88,6 @@ final class PhenodataBackupForm extends EntityForm {
     return new static(
       $container->get('entity_type.manager'),
       $container->get('config.factory'),
-      $container->get('tripal.importer'),
       $container->get('current_user'),
     );
   }
@@ -100,6 +101,20 @@ final class PhenodataBackupForm extends EntityForm {
 
     $entity = $this->getEntity();
 
+    if (!$entity->isNew() && $entity->get('user_id') != $this->user->id()) {
+      if ($this->user->hasPermission('view_all phenodata_backup')) {
+        // The user can view or modify all backups.
+        $this->messenger()
+          ->addWarning('This data file belongs to another user. Please consider notifying the owner before making significant updates.');
+
+      }
+      else {
+        // The user can only view or modify own backups. Unauthorized access.
+        $form['unauthorized']['#markup'] = 'This data file backup does not belong to this account.';
+        return $form;
+      }
+    }
+
     // Entity configuration ID number.
     $form['id'] = [
       '#type' => 'hidden',
@@ -107,91 +122,71 @@ final class PhenodataBackupForm extends EntityForm {
       '#value' => uniqid(),
     ];
 
-    if ($entity->isNew()) {
-      // Synchronize the file types defined by the importer that will be the
-      // main source of future backup data collection file.
-      $importer_plugin_definition = $this->tripal_importer
-        ->getDefinition('trpcultivate-phenotypes-share-importer');
-      $importer_file_extension = implode(' ', $importer_plugin_definition['file_types']);
+    $importer_file_extension = implode(' ', self::BACKUP_FILE_TYPES);
 
-      $config_backup_dir = $this->service_ConfigFactory
-        ->get('trpcultivate_phenotypes.settings')
-        ->get('trpcultivate.phenotypes.directory.data_backup');
-      $backup_dir = $config_backup_dir ?? 'public:://phenotype-backups/';
+    $config_backup_dir = $this->service_ConfigFactory
+      ->get('trpcultivate_phenotypes.settings')
+      ->get('trpcultivate.phenotypes.directory.data_backup');
+    $backup_dir = $config_backup_dir ?? 'public:://phenotype-backups/';
 
-      $form['backup_file'] = [
-        '#type' => 'managed_file',
-        '#title' => 'Data File',
-        '#description' => $this->t('Select data file to backup. Only [@ext] file extensions are allowed.', ['@ext' => $importer_file_extension]),
-        '#upload_location' => $backup_dir,
-        '#multiple' => FALSE,
-        '#required' => TRUE,
-        '#upload_validators' => [
-          'FileExtension' => [
-            'extensions' => $importer_file_extension,
-          ],
+    $form['backup_file'] = [
+      '#type' => 'managed_file',
+      '#title' => 'Data File',
+      '#description' => $this->t('Select data file to backup. Only [@ext] file extensions are allowed.', ['@ext' => $importer_file_extension]),
+      '#upload_location' => $backup_dir,
+      '#multiple' => FALSE,
+      '#required' => TRUE,
+      '#upload_validators' => [
+        'FileExtension' => [
+          'extensions' => $importer_file_extension,
         ],
-      ];
+      ],
+    ];
 
-      // Restrict project autocomplete suggestions to project used as
-      // research_experiment Tripal content type.
-      $entity_type = $this->service_EntityTypeManager
-        ->getStorage('tripal_entity_type')
-        ->load('research_experiment');
+    if (!$entity->isNew()) {
+      $fids = $entity->get('file_id');
+      $fids = (is_string($fids)) ? [$fids] : $fids;
 
-      $type_id = 0;
-      if ($entity_type) {
-        $entity_type_term_internalid = $entity_type->getTerm()
-          ->getInternalId();
-
-        // 0 value will suggest all projects.
-        $type_id = $entity_type_term_internalid;
-      }
-
-      $form['project_name'] = [
-        '#type' => 'textfield',
-        '#title' => 'Project/Experiment Name',
-        '#description' => $this->t('Select the project name the data file is specific to.'),
-        '#description_display' => 'after',
-        '#required' => TRUE,
-        '#attributes' => ['placeholder' => 'Project/Experiment Name'],
-        '#autocomplete_route_name' => 'tripal_chado.generic_autocomplete',
-        '#autocomplete_route_parameters' => [
-          'type_id' => $type_id,
-          'match_limit' => 5,
-          'base_table' => 'project',
-          'column_name' => 'name',
-          'type_column' => 'type_id',
-          'property_table' => 'project',
-        ],
-      ];
+      $form['backup_file']['#value'] = ['fids' => $fids];
+      $form['backup_file']['#disabled'] = TRUE;
     }
-    else {
-      // Ensure that user can only modify data file that they own.
-      if ($entity->get('user_id') != $this->user->id()) {
-        $form['not_my_backup'] = [
-          '#markup' => 'This data file backup does not belong to this account.',
-        ];
 
-        return $form;
-      }
+    // Restrict project autocomplete suggestions to project used as
+    // research_experiment Tripal content type.
+    $entity_type = $this->service_EntityTypeManager
+      ->getStorage('tripal_entity_type')
+      ->load('research_experiment');
 
-      $file_id = $entity->get('file_id');
-      $file_obj = $this->service_EntityTypeManager
-        ->getStorage('file')
-        ->load($file_id);
+    $type_id = 0;
+    if ($entity_type) {
+      $entity_type_term_internalid = $entity_type->getTerm()
+        ->getInternalId();
 
-      $url = $file_obj->createFileUrl();
-      $filename = $file_obj->getFileName();
-
-      $project_id = (int) $entity->get('project_id');
-      $project_name = ChadoProjectAutocompleteController::getProjectName($project_id);
-
-      $form['backup_file'] = [
-        '#markup' => '<p>Data File: <a target="_blank" href="' . $url . '">' . $filename . '</a><br />' .
-        'Project/Experiment: ' . $project_name . '</p>',
-      ];
+      // 0 value will suggest all projects.
+      $type_id = $entity_type_term_internalid;
     }
+
+    $project_id = (int) $entity->get('project_id');
+    $project_name = ($project_id) ? ChadoProjectAutocompleteController::getProjectName($project_id) : '';
+    $form['project_name'] = [
+      '#type' => 'textfield',
+      '#title' => 'Research Experiment',
+      '#description' => $this->t('Select the project name the data file is specific to.'),
+      '#description_display' => 'after',
+      '#required' => TRUE,
+      '#attributes' => ['placeholder' => 'Project/Experiment Name'],
+      '#autocomplete_route_name' => 'tripal_chado.generic_autocomplete',
+      '#autocomplete_route_parameters' => [
+        'type_id' => $type_id,
+        'match_limit' => 5,
+        'base_table' => 'project',
+        'column_name' => 'name',
+        'type_column' => 'type_id',
+        'property_table' => 'project',
+      ],
+      '#default_value' => ($project_id) ? "$project_name ($project_id)" : NULL,
+      '#disabled' => ($project_id) ? TRUE : FALSE,
+    ];
 
     $form['comments'] = [
       '#type' => 'textarea',
@@ -234,7 +229,7 @@ final class PhenodataBackupForm extends EntityForm {
       }
 
       if ($invalid_project) {
-        $form_state->setErrorByName('project_name', 'The project is not recognized. Please select a project and try again.');
+        $form_state->setErrorByName('project_name', 'The Research Experiment name is not recognized. Please select an option and try again.');
       }
     }
 
@@ -307,7 +302,6 @@ final class PhenodataBackupForm extends EntityForm {
   protected function actionsElement(array $form, FormStateInterface $form_state) {
 
     $element = $this->actions($form, $form_state);
-    $entity = $this->getEntity();
 
     // The listing does not provide a delete action, catch the delete button
     // in edit mode and remove it.
@@ -316,7 +310,7 @@ final class PhenodataBackupForm extends EntityForm {
     }
 
     // A user is attempting to modify an entity that belongs to someone else.
-    if (!$entity->isNew() && $entity->get('user_id') != $this->user->id()) {
+    if (isset($form['unauthorized'])) {
       // Submit == Save.
       unset($element['submit']);
     }
