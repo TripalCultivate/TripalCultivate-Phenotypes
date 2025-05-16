f3d0ec333efc7807308699aebd7cadbfbcd0c6ef
RLT <reynoldltan@gmail.com>
2025-05-16 09:48:31 -0600

    Adds comment to genus check

--- trpcultivate_phenotypes/src/Plugin/Validators/ProjectGenusMatch.php
+++ trpcultivate_phenotypes/src/Plugin/Validators/ProjectGenusMatch.php
@@ -141,6 +141,7 @@ class ProjectGenusMatch extends TripalCultivatePhenotypesValidatorBase implement
         $genus_match = FALSE;
 
         foreach ($project_genus as $value) {
+          // Project genus has one or more genus set.
           $g = (is_array($value)) ? $value['genus'] : $value;
 
           if ($g == $genus) {
