f00ebc6e5b146a5b7a2529a43f7b20a9f81c756c
RLT <reynoldltan@gmail.com>
2025-08-26 09:23:14 -0600

    Fixes failed test

--- trpcultivate_phenoshare/src/Plugin/TripalImporter/TripalCultivatePhenoShareImporter.php
+++ trpcultivate_phenoshare/src/Plugin/TripalImporter/TripalCultivatePhenoShareImporter.php
@@ -356,7 +356,10 @@ class TripalCultivatePhenoShareImporter extends ChadoImporterBase implements Con
     $validators['data-row']['empty_cell'] = $instance;
     // - All germplasm names exist in the database
     $instance = $this->service_validatorPluginManager->createInstance('germplasm_name_exists');
-    $instance->setIndices($header_index['Germplasm Name']);
+    $indices = [
+      $header_index['Germplasm Name'],
+    ];
+    $instance->setIndices($indices);
     $instance->setGenus($form_values['genus']);
     $validators['data-row']['germplasm_name_exists'] = $instance;
 
