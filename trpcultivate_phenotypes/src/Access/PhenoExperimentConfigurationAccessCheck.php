392e74c5bb9a8ffa1e262b212f091ca0a2970cc2
RLT <reynoldltan@gmail.com>
2025-09-04 11:15:59 -0600

    Setup Phenotypes tab with access check

---
+++ trpcultivate_phenotypes/src/Access/PhenoExperimentConfigurationAccessCheck.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\trpcultivate_phenotypes\Access;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Routing\Access\AccessInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\tripal\Entity\TripalEntity;
+
+/**
+ * Checks that only allowed_content_type gets the Phenotypes tab.
+ */
+class PhenoExperimentConfigurationAccessCheck implements AccessInterface {
+
+  /**
+   * Array of Tripal content type bundle name to show Phenotypes tab element.
+   *
+   * @var array
+   */
+  private const ALLOWED_CONTENT_TYPE = [
+    'research_experiment',
+  ];
+
+  /**
+   * Access check.
+   *
+   * @param \Drupal\tripal\Entity\TripalEntity $tripal_entity
+   *   Tripal entity.
+   * @param use Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account. This is the user requesting access.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public function access(TripalEntity $tripal_entity, AccountInterface $account) {
+
+    return (in_array($tripal_entity->bundle(), self::ALLOWED_CONTENT_TYPE))
+      ? AccessResult::allowed()
+      : AccessResult::forbidden();
+  }
+}
---
+++ trpcultivate_phenotypes/src/Form/PhenoExperimentConfigurationForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\trpcultivate_phenotypes\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class definition of Experiment Configuration page.
+ */
+class PhenoExperimentConfigurationForm extends FormBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getFormId() {
+    return 'content_bio_data_experiment_configure_form';
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+}
