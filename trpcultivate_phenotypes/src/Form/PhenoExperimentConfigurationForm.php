3ecee72f128c4ebd2bb62f7a9bbf036169210d5b
RLT <reynoldltan@gmail.com>
2025-09-05 10:50:54 -0600

    Setup form structure

--- trpcultivate_phenotypes/src/Form/PhenoExperimentConfigurationForm.php
+++ trpcultivate_phenotypes/src/Form/PhenoExperimentConfigurationForm.php
@@ -4,11 +4,43 @@ namespace Drupal\trpcultivate_phenotypes\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\tripal_chado\Database\ChadoConnection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class definition of Experiment Configuration page.
  */
 class PhenoExperimentConfigurationForm extends FormBase {
+
+  /**
+   * A Database query interface for querying Chado using Tripal DBX.
+   *
+   * @var \Drupal\tripal_chado\Database\ChadoConnection
+   */
+  protected ChadoConnection $chado_connection;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\tripal_chado\Database\ChadoConnection $chado_connection
+   *   The connection to the Chado database.
+   */
+  public function __construct(
+    ChadoConnection $chado_connection,
+  ) {
+
+    $this->chado_connection = $chado_connection;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+
+    return new static(
+      $container->get('tripal_chado.database'),
+    );
+  }
 
   /**
    * {@inheritDoc}
@@ -22,6 +54,10 @@ class PhenoExperimentConfigurationForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+    $this->messenger()
+      ->addWarning('A Trait cannot be modified or removed from an Experiment once phenotypic data has been associated with it.');
+
+    return $form;
   }
 
   /**
