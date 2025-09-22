82d8054bd402b81aced37edcb36aaa3eb724fa9a
RLT <reynoldltan@gmail.com>
2025-09-22 10:20:09 -0600

    Setup automated test

--- trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentConfigurationFormTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentConfigurationFormTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\trpcultivate_phenotypes\Kernel\Forms;
 
 use Drupal\Core\Url;
-use Drupal\Component\Utility\Random;
 use Drupal\Core\Form\FormState;
 use Drupal\Core\Routing\RouteMatch;
 use Drupal\Core\Routing\RouteObjectInterface;
@@ -67,7 +66,7 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
   const ROUTE_NAME = 'trpcultivate_phenotypes.experiment_configuration';
 
   /**
-   * Test genus and trait set.
+   * Test genus with a set of test traits.
    *
    * @var array
    */
@@ -150,7 +149,6 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
       'id' => uniqid(),
       'type' => 'research_experiment',
       'label' => $experiment_name,
-      'title' => $experiment_name,
     ]);
 
     $this->research_experiment_entity
@@ -228,7 +226,7 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
     $this->assertEquals(
       'content_bio_data_research_experiment_configure_form',
       PhenoExperimentConfigurationForm::create($this->container)->getFormId(),
-      'The form id returned does not match expected form id of experiment configuration form'
+      'The form id returned does not match expected form id of experiment configuration form.'
     );
   }
 
@@ -238,7 +236,6 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
   public function testPageAccess() {
 
     $http_kernel_service = $this->container->get('http_kernel');
-    $random = new Random();
 
     $page_url = Url::fromRoute(self::ROUTE_NAME, [
       'tripal_entity' => $this->research_experiment_entity->id(),
@@ -250,29 +247,51 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
     // 403 - unauthorized access.
     // 200 - Ok.
     // @see trpcultivate_phenotypes.experiment_configuration route.
-    $permissions = [
-      '' => 403,
-      'access content' => 403,
-      'manage tripal jobs' => 403,
-      'administer tripal content' => 200,
+    $route = $this->container->get('router.route_provider')
+      ->getRouteByName(self::ROUTE_NAME);
+
+    $users = [
+      0 => [
+        'name' => 'Anonymous',
+        'permissions' => [],
+        'access' => 403,
+        'is_admin' => FALSE,
+      ],
+      1 => [
+        'name' => 'Administrator',
+        'permissions' => [],
+        'access' => 200,
+        'is_admin' => TRUE,
+      ],
+      2 => [
+        'name' => 'Authenticated User',
+        'permissions' => ['access content'],
+        'access' => 403,
+        'is_admin' => FALSE,
+      ],
+      3 => [
+        'name' => 'Tripal Content Administrator',
+        'permissions' => [$route->getRequirements()['_permission']],
+        'access' => 200,
+        'is_admin' => FALSE,
+      ],
     ];
 
-    $i = 0;
-    foreach ($permissions as $permission => $access_code) {
-      // Create a user with the permission.
-      $new_user = $this->createUser(($permission) ? [$permission] : [], $random->name(), FALSE, ['uid' => $i]);
-      $this->assertNotEquals($new_user->id(), 1, 'Test user must not have the magic user id of 1');
-      $this->setCurrentUser($new_user);
+    foreach ($users as $uid => $user) {
+      $new_user = $this->createUser($user['permissions'], $user['name'], $user['is_admin'], ['uid' => $uid]);
+      if (!$user['is_admin']) {
+        // Ensure non-administrator account was not assigned 1 as user id.
+        $this->assertNotEquals($new_user->id(), 1, 'Test user must not have the magic user id of 1');
+      }
 
+      $this->setCurrentUser($new_user);
       $request = Request::create($page_url);
+
       $this->assertEquals(
-        $access_code,
+        $user['access'],
         $http_kernel_service->handle($request)->getStatusCode(),
         'User access permission does not match expected access permission.'
       );
-
-      user_logout();
-      $i += 2;
     }
   }
 
@@ -281,24 +300,24 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
    */
   public function testBuildForm() {
 
-    $form = [];
-    $form_state = new FormState();
+    $route = $this->container->get('router.route_provider')
+      ->getRouteByName(self::ROUTE_NAME);
+
+    $this->setCurrentUser(
+      $this->createUser([$route->getRequirements()['_permission']])
+    );
 
     // Experiment configuration form.
     // Prepare the route with slug value before loading the form.
     // @see drupal/core/tests/Drupal/Tests/Core/Routing/RouteMatchTest.php
-    $route_path = $this->container->get('router.route_provider')
-      ->getRouteByName(self::ROUTE_NAME);
-
     $request = new Request();
     $request->attributes->set(RouteObjectInterface::ROUTE_NAME, self::ROUTE_NAME);
-    $request->attributes->set(RouteObjectInterface::ROUTE_OBJECT, $route_path);
+    $request->attributes->set(RouteObjectInterface::ROUTE_OBJECT, $route);
     $request->attributes->set('tripal_entity', $this->research_experiment_entity);
-    $route_match = RouteMatch::createFromRequest($request);
-    $this->container->set('current_route_match', $route_match);
+    $this->container->set('current_route_match', RouteMatch::createFromRequest($request));
 
     $config_form = PhenoExperimentConfigurationForm::create($this->container)
-      ->buildForm($form, $form_state);
+      ->buildForm([], new FormState());
 
     ['record_id' => $experiment_id, 'value' => $experiment_name] = $this->research_experiment_entity->get('exp_name')->getValue()[0];
 
@@ -371,39 +390,43 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
    */
   public function testPageMarkup() {
 
-    $http_kernel_service = $this->container->get('http_kernel');
+    $route = $this->container->get('router.route_provider')
+      ->getRouteByName(self::ROUTE_NAME);
 
-    // All genus.
+    $this->setCurrentUser(
+      $this->createUser([$route->getRequirements()['_permission']])
+    );
+
     $page_url = Url::fromRoute(self::ROUTE_NAME, [
       'tripal_entity' => $this->research_experiment_entity->id(),
       'genus' => 0,
     ])
       ->toString();
 
-    $user = $this->createUser(['administer tripal content'], 'TripalAdmin');
-    $this->setCurrentUser($user);
-
     $request = Request::create($page_url);
-    $page = $http_kernel_service->handle($request)->getContent();
+    $page = $this->container->get('http_kernel')->handle($request)
+      ->getContent();
 
-    ['record_id' => $eperiment_id, 'value' => $experiment_name] = $this->research_experiment_entity->get('exp_name')
+    ['record_id' => $experiment_id, 'value' => $experiment_name] = $this->research_experiment_entity->get('exp_name')
       ->getValue()[0];
 
     $this->assertStringContainsString(
       'Phenotypes: ' . $experiment_name,
       (string) $page,
-      'The page does not contain the expected page title.'
+      'The page does not contain the expected page title containing the experiment name.'
     );
 
-    $genus_map = [];
     $genus_ontology_service = $this->container->get('trpcultivate_phenotypes.genus_ontology');
+
+    $genus_map = [];
     foreach (array_keys($this->trait_set) as $genus) {
+      $genus = explode(':', $genus)[0];
       $cv_id = $genus_ontology_service->getGenusOntologyConfigValues($genus)['trait'];
       $genus_map[$cv_id] = $genus;
     }
 
-    $this->setRawContent($page);
-
+    // Using the same query setup, test that the expected row is at the same
+    // table item row number in the markup.
     $query = $this->chado_connection->select('trpcultivate_phenocombo', 'tc');
     $query->join('1:cvterm', 't', 'tc.attr_id = t.cvterm_id');
     $query->join('1:cv', 'v', 't.cv_id = v.cv_id');
@@ -427,26 +450,33 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
 
     $query_result = $query->execute();
 
-    $trait_service = $this->container->get('trpcultivate_phenotypes.traits');
+    $this->setRawContent($page);
+    $table_rows = $this->cssSelect('tbody tr');
 
-    // Using the same query setup, test that the expected row is at the same
-    // table item row number in the markup.
-    $genus_class = [];
     $status_class = [
-      'tcp-pheno-required',
-      'tcp-pheno-shared',
-      'tcp-pheno-collected',
       'tcp-pheno-archived',
+      'tcp-pheno-required',
+      'tcp-pheno-collected',
+      'tcp-pheno-shared',
     ];
 
+    $trait_service = $this->container->get('trpcultivate_phenotypes.traits');
+
+    $genus_set = [];
     foreach ($query_result as $i => $trait_row) {
       $genus = $genus_map[$trait_row->cv_id];
-      if (!in_array($genus_class, $genus)) {
+      if (!in_array($genus, $genus_set)) {
         $trait_service->setTraitGenus($genus);
-
-        $class = ($i == 0) ? 'tcp-group-shade-light' : 'tcp-group-shade-dark';
-        $table_rows = $this->cssSelect("tbody tr.$class");
+        array_push($genus_set, $genus);
       }
+
+      $current_row = $table_rows[$i]->asXML();
+
+      $this->assertStringContainsString(
+        $trait_row->label,
+        $current_row,
+        'The trait combo label ' . $trait_row->label . ' is expected in table row #' . $i
+      );
 
       ['trait' => $trait, 'method' => $method, 'unit' => $unit] = $trait_service->getTraitMethodUnitCombo(
         $trait_row->attr_id,
@@ -455,50 +485,102 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
       );
 
       $this->assertStringContainsString(
-        $trait,
-        (string) $table_rows[$i][0],
-        'The trait combo name ' . $trait . ' is expected to appear at table row number ' . $i
+        $trait->name,
+        $current_row,
+        'The trait combo name ' . $trait->name . ' is expected in table row #' . $i
       );
 
       $this->assertStringContainsString(
-        $method,
-        (string) $table_rows[$i][0],
-        'The trait combo method ' . $method . ' is expected to appear at table row number ' . $i
+        $method->name,
+        $current_row,
+        'The trait combo method ' . $method->name . ' is expected in table row #' . $i
       );
 
       $this->assertStringContainsString(
-        $unit,
-        (string) $table_rows[$i][0],
-        'The trait combo unit ' . $unit . ' is expected to appear at table row number ' . $i
-      );
-
-      $this->assertStringContainsString(
-        $trait_row->label,
-        (string) $table_rows[$i][0],
-        'The trait combo label ' . $trait_row->label . ' is expected to appear at table row number ' . $i
-      );
-
-      $this->assertStringContainsString(
-        $remove = ($trait_row->is_archived || $trait_row->was_collected || $trait_row->was_shared) ? '-' : 'x',
-        (string) $table_rows[$i][0],
-        'The trait combo is expected to have ' . $remove . ' for the remove trait option.'
+        $unit->name,
+        $current_row,
+        'The trait combo unit ' . $unit->name . ' is expected in table row #' . $i
       );
 
       $trait_status = [
         $trait_row->is_archived,
         $trait_row->is_required,
         $trait_row->was_collected,
-        $trait_row->was_share,
+        $trait_row->was_shared,
       ];
 
-      foreach ($trait_status as $i => $status) {
-        if ($status == 1) {
+      foreach ($trait_status as $j => $status) {
+        if ($status) {
+          // A value 1 will insert a coressponding trait status icon.
           $this->assertStringContainsString(
-            $status_class[$i],
-            (string) $table_rows[$i][0],
-            'The trait combo is expected to have the icon status css class name of ' . $status_class[$i]
+            $status_class[$j],
+            $current_row,
+            'The trait combo is expected to have the icon status CSS class name ' . $status_class[$j] . ' in table row #' . $i
           );
         }
+        else {
+          $this->assertStringNotContainsString(
+            $status_class[$j],
+            $current_row,
+            'The trait combo is expected not to have the icon status CSS class name ' . $status_class[$j] . ' in table row #' . $i
+          );
+        }
+      }
+
+      $this->assertStringContainsString(
+        $remove = ($trait_row->is_archived || $trait_row->was_collected || $trait_row->was_shared) ? '-' : 'x',
+        $current_row,
+        'The trait combo is expected to have the character ' . $remove . ' as the remove trait option in table row #' . $i
+      );
+    }
+  }
+
+  /**
+   * Test genus filter.
+   */
+  public function testGenusFilter() {
+
+    $route = $this->container->get('router.route_provider')
+      ->getRouteByName(self::ROUTE_NAME);
+
+    $this->setCurrentUser(
+      $this->createUser([$route->getRequirements()['_permission']])
+    );
+
+    $summary_table_name = 'experiment_traits_summary_table';
+    $all_trait = [];
+
+    // Switch between the genus.
+    foreach ($this->trait_set as $organism => $traits) {
+      $genus = explode(':', $organism)[0];
+
+      $request = new Request();
+      $request->attributes->set(RouteObjectInterface::ROUTE_NAME, self::ROUTE_NAME);
+      $request->attributes->set(RouteObjectInterface::ROUTE_OBJECT, $route);
+      $request->attributes->set('tripal_entity', $this->research_experiment_entity);
+      $request->attributes->set('genus', $genus);
+      $this->container->set('current_route_match', RouteMatch::createFromRequest($request));
+
+      $config_form = PhenoExperimentConfigurationForm::create($this->container)
+        ->buildForm([], new FormState());
+
+      $this->assertEquals(
+        $config_form[$summary_table_name]['#header']['trait_combo']['data']['#value'],
+        $genus,
+        'The default selected option of the filter genus field does not match expected value.'
+      );
+
+      // All the trait names in a genus.
+      $all_trait[$genus] = array_map(function ($t) {
+        return $t['Trait Name'];
+      }, $traits);
+
+      foreach ($config_form[$summary_table_name]['#rows'] as $row) {
+        $this->assertContains(
+          $trait_name = $row['data'][1]['data']['#props']['name'],
+          $all_trait[$genus],
+          'The trait name ' . $trait_name . ' is expected in genus ' . $genus . ' filter result.'
+        );
       }
     }
   }
