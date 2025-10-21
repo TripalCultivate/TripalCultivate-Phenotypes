f4a845d67f88a455192358a0828ad3cc9ddb28f3
RLT <reynoldltan@gmail.com>
2025-10-21 07:21:48 -0600

    Adds event listener to add trait button

---
+++ trpcultivate_phenotypes/js/trpcultivate-phenotypes-script-experiment-configuration.js
@@ -0,0 +1,52 @@
+/**
+ * @file
+ * Experiment configuration behaviours.
+ */
+
+(function($, Drupal, drupalSettings) {
+  Drupal.behaviors.traitSummaryPage = {
+    attach: function (context, settings) {
+
+      var pathname = window.location.pathname;
+      var tcpSettings = drupalSettings.tcpSettings;
+
+      if (tcpSettings && !pathname.includes('/' + tcpSettings.genus)) {
+        // Update url to include the default single genus.
+        window.history.pushState({}, '', pathname + '/' + tcpSettings.genus);
+      }
+
+      // Event listener to filter summary listing by genus select field.
+      $('#tcp-filter-trait-table-by-genus')
+        .change(function() {
+
+          var selectValue = $(this).val();
+          var pathPcs = pathname.split('/');
+          var newLocation;
+
+          if (pathPcs[pathPcs.length - 1] == 'configure') {
+            newLocation = pathname + '/' + selectValue;
+          }
+          else {
+            pathPcs[pathPcs.length - 1] = selectValue;
+
+            if (selectValue == 0) {
+              pathPcs.pop();
+            }
+
+            newLocation = window.location.origin + pathPcs.join('/');
+          }
+
+          window.location.href = newLocation;
+      });
+
+      // Add event listener to Add Trait button.
+      $('#tcp-add-trait-to-experiment', context)
+        .on('traitSummaryPage')
+        .on('click', function(e) {
+
+          e.preventDefault();
+          alert();
+      });
+
+      ///
+}}}(jQuery, Drupal, drupalSettings));
