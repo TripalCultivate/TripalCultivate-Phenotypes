32555f1f9f80de1169e8079387a7dd5771786834
Reynold <reynold@reynolds-imac.lan>
2026-02-03 16:53:05 -0600

    Rename link title

--- trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentTraitSelectorFormTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentTraitSelectorFormTest.php
@@ -304,13 +304,13 @@ class PhenoExperimentTraitSelectorFormTest extends ChadoTestKernelBase {
 
     $this->assertEquals(
       'link',
-      $search_toolbar['suggest']['#type'],
+      $search_toolbar['import_traits']['#type'],
       'The trait selector form is expected to contain a link element.',
     );
 
     $this->assertStringContainsString(
-      $title = 'Suggest a Trait',
-      $search_toolbar['suggest']['#title'],
+      $title = 'Import Traits',
+      $search_toolbar['import_traits']['#title'],
       'The trait selector form is expected to contain a link titled ' . $title,
     );
 
