1b13591d148496f497cffa6b744943dbbb64e07b
RLT <reynoldltan@gmail.com>
2026-02-04 09:31:54 -0600

    Update trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentConfigurationFormTest.php
    Co-authored-by: Vidusha Wilpita <132868285+vidu-wil@users.noreply.github.com>

--- trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentConfigurationFormTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentConfigurationFormTest.php
@@ -407,7 +407,7 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
     $this->assertStringContainsString(
       'Trait Method Unit',
       (string) $config_form[$summary_table_name]['#prefix'],
-      'The summary table title text does not matche epected title.',
+      'The summary table title text does not match the expected title.',
     );
 
     $db_trait_count = $this->container->get('database')
