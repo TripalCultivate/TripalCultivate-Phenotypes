fa84372a21ebe4ddfdd72b23601b15b5e7844f69
RLT <reynoldltan@gmail.com>
2026-02-23 06:50:39 -0600

    Fix failed test

--- trpcultivate_phenotypes/tests/src/Kernel/TripalImporter/TraitImporterProcessValidationTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/TripalImporter/TraitImporterProcessValidationTest.php
@@ -144,13 +144,16 @@ class TraitImporterProcessValidationTest extends ChadoTestKernelBase {
       'trpcultivate-phenotypes-traits-importer',
       $this->definitions,
       $this->chado_connection,
+      $container->get('messenger'),
+      $container->get('tripal.logger'),
+      $container->get('tripal.fileretriever'),
+      $container->get('tripal.backend_publish'),
       $this->container->get('trpcultivate_phenotypes.genus_ontology'),
       $this->container->get('trpcultivate_phenotypes.traits'),
       $this->container->get('plugin.manager.trpcultivate_validator'),
       $this->container->get('trpcultivate.template_generator'),
       $this->container->get('entity_type.manager'),
       $this->renderer,
-      $this->container->get('messenger'),
     );
   }
 
