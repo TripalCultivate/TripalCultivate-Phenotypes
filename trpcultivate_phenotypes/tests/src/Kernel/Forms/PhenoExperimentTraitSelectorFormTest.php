ab5f914cdaf6b96a01f42435b39e658efa536875
RLT <reynoldltan@gmail.com>
2026-05-25 07:01:19 -0600

    Fixes trait selector failed test

--- trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentTraitSelectorFormTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentTraitSelectorFormTest.php
@@ -14,6 +14,8 @@ use Drupal\tripal\Entity\TripalEntity;
 use Drupal\tripal\Services\TripalLogger;
 use Drupal\tripal_chado\Database\ChadoConnection;
 use Drupal\trpcultivate_phenotypes\Form\PhenoExperimentTraitSelectorForm;
+use Drupal\trpcultivate_phenotypes\Service\TripalCultivatePhenotypesGenusOntologyService;
+use Drupal\trpcultivate_phenotypes\Service\TripalCultivatePhenotypesTermsService;
 use Symfony\Component\HttpFoundation\Request;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
@@ -166,6 +168,34 @@ class PhenoExperimentTraitSelectorFormTest extends ChadoTestKernelBase {
 
     $config_terms = $this->setTermConfig();
 
+    $default_terms = \Drupal::configFactory()->getEditable('trpcultivate_phenotypes.settings')
+      ->get('trpcultivate.default_terms.term_set');
+
+    $terms = [];
+    foreach ($default_terms as $cv) {
+      foreach ($cv['terms'] as $term_set) {
+        $term_set['cv'] = ['name' => $cv['name'], 'definition' => $cv['definition']];
+        $terms[$term_set['config_map']] = $term_set;
+      }
+    }
+
+    $mock_terms_service = $this->getMockBuilder(TripalCultivatePhenotypesTermsService::class)
+      ->setConstructorArgs([
+        $this->container->get('config.factory'),
+        $this->container->get('tripal_chado.chado_buddy'),
+        $this->container->get('tripal.logger'),
+      ])
+      ->onlyMethods(['defineTerms', 'getTermId'])
+      ->getMock();
+
+    $mock_terms_service->method('defineTerms')
+      ->willReturn($terms);
+
+    $mock_terms_service->method('getTermId')
+      ->willReturn(1);
+
+    $this->container->set('trpcultivate_phenotypes.terms', $mock_terms_service);
+
     // Create test records.
     $experiment = 'A Good Research Experiment';
     $genus = array_keys($this->trait_set)[0];
@@ -176,6 +206,15 @@ class PhenoExperimentTraitSelectorFormTest extends ChadoTestKernelBase {
       ->values(['name' => $experiment])
       ->execute();
 
+    $mock_ontology_service = $this->getMockBuilder(TripalCultivatePhenotypesGenusOntologyService::class)
+      ->setConstructorArgs([
+        $this->container->get('config.factory'),
+        $this->container->get('tripal_chado.database'),
+        $this->container->get('tripal.logger'),
+      ])
+      ->onlyMethods(['getGenusOntologyConfigValues'])
+      ->getMock();
+
     $entity = TripalEntity::create([
       'id' => 1,
       'type' => 'research_experiment',
@@ -184,6 +223,7 @@ class PhenoExperimentTraitSelectorFormTest extends ChadoTestKernelBase {
 
     $entity->set('exp_name', ['record_id' => $project_id, 'value' => $experiment]);
 
+    $mock_return_map = [];
     // Pair both test genus to the experiment.
     foreach (array_keys($this->trait_set) as $i => $ins_genus) {
       $this->chado_connection->insert('1:organism')
@@ -195,7 +235,13 @@ class PhenoExperimentTraitSelectorFormTest extends ChadoTestKernelBase {
         ])
         ->execute();
 
-      $this->setOntologyConfig($ins_genus);
+      $config_genus = $this->setOntologyConfig($ins_genus);
+      $genus_config = [];
+      foreach ($config_genus as $config => $config_value) {
+        $genus_config[$config] = ($config == 'database') ? $config_value['db_id'] : $config_value['cv_id'];
+      }
+
+      $mock_return_map[$ins_genus] = $genus_config;
 
       $this->chado_connection->insert('1:projectprop')
         ->fields([
@@ -213,6 +259,14 @@ class PhenoExperimentTraitSelectorFormTest extends ChadoTestKernelBase {
 
     // Set this class property to reference the experiment entity created.
     $this->exp_entity = $entity;
+
+    $mock_ontology_service->method('getGenusOntologyConfigValues')
+      ->willReturnMap([
+      ['Lens', $mock_return_map['Lens']],
+      ['Triticum', $mock_return_map['Triticum']],
+      ]);
+
+    $this->container->set('trpcultivate_phenotypes.genus_ontology', $mock_ontology_service);
 
     // Install trait combos.
     $trait_service = $this->container->get('trpcultivate_phenotypes.traits');
