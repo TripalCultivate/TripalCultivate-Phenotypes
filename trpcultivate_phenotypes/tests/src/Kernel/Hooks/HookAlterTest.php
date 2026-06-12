de5fbbc9f25ecc06fe386bbe95a66a53aa0fc4bb
RLT <reynoldltan@gmail.com>
2026-06-12 08:55:10 -0600

    Fix failed test - mismatch validation error message

--- trpcultivate_phenotypes/tests/src/Kernel/Hooks/HookAlterTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/Hooks/HookAlterTest.php
@@ -343,7 +343,7 @@ class HookAlterTest extends ChadoTestKernelBase {
 
     $constraint_message = strtr($constraint->genus_failed, [
       '%genus' => $exp_genus,
-      '%content-type' => $this->exp_entity->label(),
+      '%content-type' => $this->exp_entity->getBundle()->label(),
       '@reload' => Link::fromTextAndUrl('Restore Values', Url::fromRoute('<current>'))->toString(),
     ]);
 
@@ -376,7 +376,7 @@ class HookAlterTest extends ChadoTestKernelBase {
     $constraint_validator->validate($this->exp_entity, $constraint);
 
     $constraint_message = strtr($constraint->all_genus_failed, [
-      '%content-type' => $this->exp_entity->label(),
+      '%content-type' => $this->exp_entity->getBundle()->label(),
       '@reload' => Link::fromTextAndUrl('Restore Values', Url::fromRoute('<current>'))->toString(),
     ]);
 
