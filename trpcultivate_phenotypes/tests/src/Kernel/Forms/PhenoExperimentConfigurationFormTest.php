7b3fe2b06d8127406185172acd1c387d6db37dd1
RLT <reynoldltan@gmail.com>
2026-06-24 19:07:38 -0600

    Update reference to field containing genus

--- trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentConfigurationFormTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/Forms/PhenoExperimentConfigurationFormTest.php
@@ -90,6 +90,13 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
   const PHENO_COMBO_TABLE = 'trpcultivate_phenocombo';
 
   /**
+   * The name of the field that contains the genus.
+   *
+   * @var string
+   */
+  const FIELD_ORGANISM = 'exp_organism';
+
+  /**
    * Test genus with a set of test traits.
    *
    * @var array
@@ -234,10 +241,8 @@ class PhenoExperimentConfigurationFormTest extends ChadoTestKernelBase {
             ])
             ->execute();
 
-          if ($entity->hasField('exp_germgenus')) {
-            $entity
-              ->set('exp_germgenus', ['record_id' => $project_id, 'value' => $genus]);
-          }
+          $entity
+            ->set(self::FIELD_ORGANISM, ['record_id' => $project_id, 'genus_value' => $genus]);
         }
       }
 
