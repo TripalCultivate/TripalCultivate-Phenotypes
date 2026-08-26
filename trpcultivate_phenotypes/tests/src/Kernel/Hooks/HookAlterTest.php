6c4550c9b6ae56fb8e2e503f92fc4940a31a2a0a
RLT <reynoldltan@gmail.com>
2026-08-26 07:10:40 -0600

    Improve code coverage

--- trpcultivate_phenotypes/tests/src/Kernel/Hooks/HookAlterTest.php
+++ trpcultivate_phenotypes/tests/src/Kernel/Hooks/HookAlterTest.php
@@ -4,16 +4,19 @@ namespace Drupal\Tests\trpcultivate_phenotypes\Kernel\Hooks;
 
 use Drupal\Core\Form\FormState;
 use Drupal\Core\Link;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Url;
 use Drupal\Tests\tripal_chado\Kernel\ChadoTestKernelBase;
 use Drupal\Tests\trpcultivate_phenotypes\Traits\PhenotypeImporterTestTrait;
 use Drupal\Tests\user\Traits\UserCreationTrait;
 use Drupal\tripal\Entity\TripalEntity;
 use Drupal\tripal_chado\Database\ChadoConnection;
+use Drupal\trpcultivate_phenotypes\Hook\TripalCultivatePhenotypesAlterHooks;
 use Drupal\trpcultivate_phenotypes\Plugin\Validation\Constraint\LockExperimentGenusWithPhenotypes;
 use Drupal\trpcultivate_phenotypes\Plugin\Validation\Constraint\LockExperimentGenusWithPhenotypesValidator;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
+use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
@@ -257,6 +260,41 @@ class HookAlterTest extends ChadoTestKernelBase {
 
     // Setup an admin user.
     $this->setCurrentUser($this->createUser(['administer tripal']));
+  }
+
+  /**
+   * Test alter hook constructor phenotypes check.
+   */
+  public function testPhenotypeCheck() {
+
+    $service_alterhook = $this->container->get('trpcultivate_phenotypes.alter_hooks');
+    $reflection = new \ReflectionClass($service_alterhook)
+      ->getProperty('has_pheno');
+
+    $this->assertFalse(
+      $reflection->getValue($service_alterhook),
+      'AlterHook service class property that determines if a phenotype exists is set to FALSE by default.',
+    );
+
+    $route_match = $this->createMock(RouteMatchInterface::class);
+    $route_match->expects($this->once())
+      ->method('getParameters')
+      ->willReturn(
+        new ParameterBag(['tripal_entity' => $this->exp_entity])
+      );
+
+    $service_alterhook = new TripalCultivatePhenotypesAlterHooks(
+      $route_match,
+      $this->chado_connection
+    );
+
+    $reflection = new \ReflectionClass($service_alterhook)
+      ->getProperty('has_pheno');
+
+    $this->assertTrue(
+      $reflection->getValue($service_alterhook),
+      'AlterHook service failed to set a class property that determines if a phenotype exists.',
+    );
   }
 
   /**
